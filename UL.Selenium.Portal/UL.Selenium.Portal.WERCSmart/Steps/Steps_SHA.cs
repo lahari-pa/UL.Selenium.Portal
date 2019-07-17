30a44ee4eb2b7db38a21c80f0d52980e99140045
23673@global.ul.com <Julianne Whitesell>
2019-07-17 14:48:33 -0400

    #78417,#60694 updated wine flow and fixed delay to use a method instead

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/Steps_SHA.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/Steps_SHA.cs
@@ -575,7 +575,7 @@ namespace UL.Selenium.Portal.WERCSmart.Steps
 		{
 			ProductRecertificationHistory thisProductRecertificationHistory = new ProductRecertificationHistory();
 			thisProductRecertificationHistory.Wait_for_load(30);
-			Delay.Seconds(15);
+			thisProductRecertificationHistory.WaitForTableLoad();
 			Report.Screenshot();
 			List<Product> ListOfRecertificationProducts = thisProductRecertificationHistory.GetProducts();
 			Report.Info("Found " + ListOfRecertificationProducts.Count.ToString() + " recertification products");
