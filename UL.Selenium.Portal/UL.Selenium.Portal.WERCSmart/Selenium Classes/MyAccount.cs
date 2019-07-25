90c564b066a7c71d1789cc1fabe042ddb1cd0957
23645@global.ul.com <Jacob Morse>
2019-07-25 13:48:14 -0400

    #73918 fixes

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Selenium Classes/MyAccount.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Selenium Classes/MyAccount.cs
@@ -15,11 +15,11 @@ using System.Collections.ObjectModel;
 namespace UL.Selenium.Portal.WERCSmart.Selenium_Classes
 {
 
-	class MyAccount : BaseObject
+	class MyAccount : SeleniumBaseObject
 	{
 		public const string BasePath = "//div[@id='mainBody']";
-		[FindsBy(How = How.XPath, Using = BasePath)]
-		protected override IWebElement containerElement { get; set; }
+
+		protected override By ContainerElementLocator => By.XPath(BasePath);
 
 		public string HeaderShowing()
 		{
