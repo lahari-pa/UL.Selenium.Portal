6462d63ebbb4b2dff194dc2a01d41ed96c1fe581
Anant Deshpande <48208@global.ul.com>
2019-08-29 18:39:01 -0400

    #86257,#86258,#86259,#86260,#86462,#86463 added

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/Steps_SHA.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/Steps_SHA.cs
@@ -1705,45 +1705,69 @@ namespace UL.Selenium.Portal.WERCSmart.Steps
 			}
 		}
 
-		[StepDefinition(@"In the SHA list of UPCs I should not see UPC: (.*)")]
-		public void ShaUPCList(string upc)
+		[StepDefinition(@"In the SHA list of UPCs I should (see|not see) UPC: (.*)")]
+		public void ShaUPCList(string condition,string upc)
 		{
-			// Switch to window
-			string currentHandle = SeleniumBrowser.WebBrowser.CurrentWindowHandle;
-			Context.AddToContext("MainWindowHandle", currentHandle);
-			ReadOnlyCollection<string> allHandles = SeleniumBrowser.WebBrowser.WindowHandles;
-			Report.Info("Looking for SHA Manager Product UPC window");
-			bool foundWindow = false;
-			foreach (string handle in allHandles)
+			try
 			{
-				Report.Info("Checking handle: " + handle);
-				SeleniumBrowser.WebBrowser.SwitchTo().Window(handle);
-				if (SeleniumBrowser.WebBrowser.FindElement(
-						By.XPath(".//h1[contains(text(),'WERCSmart Product ID')]"), 2) != null)
+				// Switch to window
+				string currentHandle = SeleniumBrowser.WebBrowser.CurrentWindowHandle;
+				Context.AddToContext("MainWindowHandle", currentHandle);
+				ReadOnlyCollection<string> allHandles = SeleniumBrowser.WebBrowser.WindowHandles;
+				Report.Info("Looking for SHA Manager Product UPC window");
+				bool foundWindow = false;
+				foreach (string handle in allHandles)
 				{
-					Report.Success("Tab was switched successfully!");
+					Report.Info("Checking handle: " + handle);
+					SeleniumBrowser.WebBrowser.SwitchTo().Window(handle);
+					if (SeleniumBrowser.WebBrowser.FindElement(
+							By.XPath(".//h1[contains(text(),'WERCSmart Product ID')]"), 2) != null)
+					{
+						Report.Success("Tab was switched successfully!");
+						Report.Screenshot();
+						foundWindow = true;
+						break;
+					}
+				}
+
+				if (!foundWindow)
+				{
+					Report.Failure("Failed to find the UPC List window ('SHA Manager Product UPC')");
 					Report.Screenshot();
-					foundWindow = true;
-					break;
+				}
+
+				List<SHAManagerProdcutUPC> displayedUpcs = new StudioSHAManager().GetUPCs();
+				if (displayedUpcs == null)
+				{
+					Report.Failure("Unable to fetch UPC Information from the SHA UPC window!");
+					Report.Screenshot();
+					return;
+				}
+
+				if (upc.ToLower().Contains("saved as"))
+				{
+					upc = Context
+						.GetFromContext(upc.Replace("saved as", "", StringComparison.InvariantCultureIgnoreCase).Trim())
+						.ToString();
+				}
+
+				if (condition == "see")
+				{
+					Report.IsTrue(displayedUpcs.Any(x => x.UPCNumber.Contains(upc)), "UPC: " + upc + " does not display",
+						"UPC: " + upc + " displays as expected");
+				}
+
+				if (condition == "not see")
+				{
+					Report.IsTrue(!displayedUpcs.Any(x => x.UPCNumber.Contains(upc)), "UPC: " + upc + " has not been deleted.",
+						"UPC: " + upc + " has been deleted as expected.");
 				}
 			}
-
-			if (!foundWindow)
+			catch (Exception ex)
 			{
-				Report.Failure("Failed to find the UPC List window ('SHA Manager Product UPC')");
+				Report.Failure(ex.Message);
 				Report.Screenshot();
 			}
-
-			List<SHAManagerProdcutUPC> displayedUpcs = new StudioSHAManager().GetUPCs();
-			if (upc.ToLower().Contains("saved as"))
-			{
-				upc = Context
-					.GetFromContext(upc.Replace("saved as", "", StringComparison.InvariantCultureIgnoreCase).Trim())
-					.ToString();
-			}
-
-			Report.IsTrue(!displayedUpcs.Any(x => x.UPCNumber.Contains(upc)), "UPC: " + upc + " has not been deleted.",
-				"UPC: " + upc + " has been deleted as expected.");
 		}
 
 		[StepDefinition(@"The recertification popup should show")]
