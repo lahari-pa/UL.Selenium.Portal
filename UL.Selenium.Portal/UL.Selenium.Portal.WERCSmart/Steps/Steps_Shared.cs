3e4a3db10307376b5baa7c2d55ee965a43d1fdec
Andrew Glew <andrew.glew@ul.com>
2019-09-09 13:56:47 +0100

    Minor changes

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/Steps_Shared.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/Steps_Shared.cs
@@ -8613,5 +8613,21 @@ namespace UL.Selenium.Portal.WERCSmart.Steps
 			Report.IsTrue(bothOnSceen, "Both The Alert and Prompt were not on screen at the same time", " Both the Alert and Prompt were on screen at the same time");
 
 		}
+
+		[StepDefinition(@"I confirm the Unsaved Changes Alert is displayed after waiting (.*) minutes accurate to the nearest (.*) minutes")]
+		public void ConfirmTheUnsavedChangesAlertDisplayedAfterWait(int expectedWait, int marginOfError)
+		{
+			// check if popup wasn't displayed after 'expected wait + margin' (test upper limit)
+			if (!new InactivityPopup().WaitUntilDisplayed((expectedWait * 60) + (marginOfError * 60), out int actualWait))
+			{
+				Report.Failure($"The Inactivity popup did not load after {expectedWait + marginOfError} minutes!");
+				Report.Screenshot();
+				return;
+			}
+			// check if pop up was displayed before 'expected wait - margin' (test lower limit)
+			Report.IsTrue(actualWait >= (expectedWait * 60) - (marginOfError * 60),
+				"The Inactivity popup did not load within the expected time frame! It was loaded after " + actualWait / 60 + " minutes",
+				"The Inactivity popup loaded within the expected time frame. It was loaded after: " + actualWait / 60 + " minutes");
+		}
 	}
 }
