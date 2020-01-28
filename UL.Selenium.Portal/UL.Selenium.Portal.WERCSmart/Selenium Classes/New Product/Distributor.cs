504c2afe0c4a64fb8c531c292751207eb8823530
Anant Deshpande <48208@global.ul.com>
2020-01-28 14:40:28 -0500

    #122365 distributor code

---
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Selenium Classes/New Product/Distributor.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UL.Automation.Selenium.Extensions;
+using UL.Automation.Utilities.Functions;
+using UL.Automation.Reporting.Functions;
+using OpenQA.Selenium;
+using UL.Automation.Reporting.SpecFlow.Classes;
+using UL.Selenium.Portal.WERCSmart.Selenium_Classes.New_Product;
+using TechTalk.SpecFlow;
+using System.IO;
+using UL.Automation.Selenium.Classes;
+using UL.Automation.Selenium.BaseClasses;
+using System.Collections.ObjectModel;
+using UL.Selenium.Portal.WERCSmart.Classes;
+
+namespace UL.Selenium.Portal.WERCSmart.Selenium_Classes.New_Product
+{
+	class Distributor : NewProduct
+	{
+		public string SearchIdNameField {
+			get => SeleniumBrowser.WebBrowser.FindElement(By.XPath(".//div[@class='table-search col-sm-4']//input[@id='inputGroup']"), 2).GetValue();
+			set
+			{
+				IWebElement el = SeleniumBrowser.WebBrowser.FindElement(By.XPath(".//div[@class='table-search col-sm-4']//input[@id='inputGroup']"), 2);
+				el.EnterText(value);
+				el.SendKeys(Keys.Return);
+				GeneralUtilities.Wait_for_load_finish();
+			}
+		}
+
+
+		public int ProductsInMyDistCount()
+		{
+			IList<IWebElement> productRows = SeleniumBrowser.WebBrowser.FindElements(By.XPath("//div[@id='distributor']//table[contains(@class, 'table')]//tbody/tr"), 2);
+			if (productRows == null || !productRows.Any())
+			{
+				return 0;
+			}
+			return productRows.Count(x => x.Displayed);
+		}
+
+		public bool ClickActionsApprove()
+		{
+			try
+			{
+				IList<IWebElement> distRow = SeleniumBrowser.WebBrowser.FindElements(By.XPath(".//table[@class='table']//tr"), 1).ToList();
+				IWebElement button = distRow.FirstOrDefault()?.FindElement(By.XPath("//a[contains(@class,'btn btn-link btn-xs') and contains(text(),'Approve')]"), 1);
+				return button.TryClick();
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+
+		public bool ClickExpandOnFirstRow()
+		{
+			try
+			{
+				IList<IWebElement> distRow = SeleniumBrowser.WebBrowser.FindElements(By.XPath(".//table[@class='table table-hover upc-table']//tr"), 1).ToList();
+				IWebElement button = distRow.FirstOrDefault()?.FindElement(By.XPath("//em[contains(@class,'fa fa-chevron-right')]"), 1);
+				return button.TryClick();
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+
+		public bool EditUpcInformation(UpcInformation info)
+		{
+			try
+			{
+				IWebElement container = this.containerElement.FindElement(By.XPath(".//table[@class='table table-hover upc-table']"), 2);
+				IList<IWebElement> textInputs = container.FindElements(By.XPath("//input[@type = 'text']"), 2);
+				IWebElement upcNumberField = container.FindElement(By.XPath(".//label[contains(text(),'UPC Number')]/..//input"), 2);
+				IWebElement ProductNameOnlabel = container.FindElement(By.XPath(".//label[contains(text(),'Product Name on Label')]/..//input"), 2);
+
+				if (ProductNameOnlabel == null)
+				{
+					Report.Info(@"Failed to find 'Size' input in the format ""Size (.. Ounces)""");
+					return false;
+				}
+				ProductNameOnlabel.EnterText(info.UPCName);
+
+
+				if (info.ContainerType.ToLower() != "none")
+				{
+					IWebElement containsType = container.FindElement(By.XPath(".//select[contains(@data-bind,'Container Type')]"), 2);
+					containsType.Select(info.ContainerType);
+				}
+
+				string regex = @"(.*)\((.*)\)";
+				IWebElement sizeField = (from input in textInputs
+										 let match = Regex.Match(input.GetAttribute("placeholder"), regex)
+										 where match.Success && match.Groups[1].Value.StartsWith("Size") && match.Groups[2].Value.Contains("Ounces")
+										 select input).FirstOrDefault();
+				if (sizeField == null)
+				{
+					Report.Info(@"Failed to find 'Size' input in the format ""Size (.. Ounces)""");
+					return false;
+				}
+				sizeField.EnterText(info.Size);
+				if (info.Dpci.Length > 0)
+				{
+					IWebElement dpciField = container.FindElement(By.XPath(".//input[contains(@data-bind,'value.field')]"), 2);
+					dpciField.EnterText(info.Dpci);
+				}
+				if (info.Quantity.Length > 0)
+				{
+					IWebElement quantityField = container.FindElement(By.XPath(".//input[@placeholder='Quantity']"), 2);
+					quantityField.EnterText(info.Quantity);
+				}
+
+				if (info.PackageType.Length > 0)
+				{
+					IWebElement packageField = container.FindElement(By.XPath(".//select[contains(@data-bind,'Package Type')]"), 2);
+					packageField.Select(info.PackageType);
+				}
+				return true;
+			}
+			catch (Exception ex)
+			{
+				Report.Info(ex.Message);
+				return false;
+			}
+		}
+	}
+}
