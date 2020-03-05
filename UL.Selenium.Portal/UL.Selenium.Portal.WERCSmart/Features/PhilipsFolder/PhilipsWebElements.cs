4c0f711e45d0588fb8833b98eaf8081b8cd1790d
Philip Nagel <philip.nagel@ul.com>
2020-03-05 07:36:33 -0500

    Working on ticket #120790

---
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Features/PhilipsFolder/PhilipsWebElements.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using UL.Automation.Selenium.BaseClasses;
+
+namespace UL.Selenium.Portal.WERCSmart.Features.PhilipsFolder
+{
+	class PhilipsWebElements : SeleniumBaseObject
+	{
+		protected override By ContainerElementLocator => throw new NotImplementedException();
+
+	}
+
+}
---
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Features/PhilipsFolder/StepDefinitions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace UL.Selenium.Portal.WERCSmart.Features.PhilipsFolder
+{
+	[Binding, Scope(Tag = "Philip")]
+	class StepDefinitions
+	{
+	}
+}
