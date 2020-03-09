9b161c448bb8e12a2fbc00ab32cb7349b8ee6d2f
Andrew Glew <andrew.glew@ul.com>
2020-03-09 09:09:30 +0000

    Added some waits in power desginer that allow the page to refresh fully before attemptying to change data codes/open graphics editor

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/Steps_Studio.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/Steps_Studio.cs
@@ -219,12 +219,13 @@ namespace UL.Selenium.Portal.WERCSmart.Steps
 			foreach (TableRow thisRow in table.Rows)
 			{
 				var thisStudioPowerDesignerPlusDesignMode =
-					new StudioPowerDesignerPlusDesignMode();
+					new StudioPowerDesignerPlusDesignMode();		
+				GeneralUtilities.StudioWaitForSpinner(30);
+				thisStudioPowerDesignerPlusDesignMode.Wait_for_load(30);
 				thisStudioPowerDesignerPlusDesignMode.DoubleClickDataCode(thisRow["datacode"]);
 				Delay.Seconds(15);
 				var thisGraphicEditor = new GraphicEditor();
-				Report.IsTrue(thisGraphicEditor.Wait_for_load(90), "Graphic editor has not loaded",
-					"Graphic editor has loaded.");
+				Report.IsTrue(thisGraphicEditor.Wait_for_load(90), "Graphic editor has not loaded","Graphic editor has loaded.");
 				string valueToSearchFor = "";
 
 				Report.Info("Attempting to set value: " + thisRow["value"] + " for graphic: " + thisRow["datacode"]);
