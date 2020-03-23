e37d8c84378a719897cd9fbcb70bda92c6c2a7a4
Philip Nagel <philip.nagel@ul.com>
2020-03-23 10:09:59 -0400

    Changes

---
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Philip/StepDefinitions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Web.Administration;
+using Newtonsoft.Json;
+using UL.Automation.Reporting.Functions;
+using UL.Automation.Reporting.SpecFlow.Classes;
+using TechTalk.SpecFlow;
+
+
+using System.Globalization;
+using Newtonsoft.Json.Converters;
+using System.Xml;
+using UL.Automation.Reporting;
+using UL.Automation.Selenium.Classes;
+using UL.Automation.TReVor.Classes;
+
+	namespace UL.Selenium.Portal.WERCSmart.Philip
+{
+		[Binding, Scope(Tag = "Philip")]
+		class StepDefinitions
+	{
+		[StepDefinition(@"Confirm that there is a CW column between Last Pub Date and GHS columns")]
+		public void GivenConfirmThatThereIsACWColumnBetweenLastPubDateAndGHSColumns()
+		{
+			WebElements WebElementsObject = new WebElements();
+			WebElementsObject.CheckColumn();
+		}
+
+		[StepDefinition(@"Find product that has a Y in the CW column")]
+		public void ThenFindProductThatHasAYInTheCWColumn()
+		{
+			WebElements WebElementsObject = new WebElements();
+			WebElementsObject.FindProduct();
+		}
+
+		[StepDefinition(@"Find product that has a N in the CW column")]
+		public void ThenFindProductThatHasANInTheCWColumn()
+		{
+			WebElements WebElementsObject = new WebElements();
+			WebElementsObject.FindProductN();
+		}
+
+
+		[StepDefinition(@"I click vendor section")]
+		public void ThenIClickVendorSection()
+		{
+			WebElements WebElementsObject = new WebElements();
+			WebElementsObject.ClickVendorSection();
+		}
+
+		[StepDefinition(@"I click a section")]
+		public void ThenIClickASection()
+		{
+			WebElements WebElementsObject = new WebElements();
+			WebElementsObject.ClickASection();
+		}
+
+		[StepDefinition(@"check text")]
+		public void ThenCheckText()
+		{
+			WebElements WebElementsObject = new WebElements();
+			WebElementsObject.CheckText();
+		}
+
+	}
+}
---
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Philip/WebElements.cs
@@ -0,0 +1,69 @@
+﻿using UL.Automation.Selenium.BaseClasses;
+using UL.Automation.Selenium.Extensions;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace UL.Selenium.Portal.WERCSmart.Philip
+{
+	class WebElements : SeleniumBaseObject
+	{
+
+		public const string BasePath = "";
+
+		protected override By ContainerElementLocator => throw new System.NotImplementedException();
+
+		public bool CheckColumn()
+		{
+			IWebElement el = this.containerElement.FindElement(By.XPath("//div[text()='CW']/../preceding-sibling::th//div[text()='Last Pub Date']"), 2);
+			IWebElement el1 = this.containerElement.FindElement(By.XPath("//div[text()='CW']/../preceding-sibling::th//div[text()='GHS']"), 2);
+			if (el != null && el1 != null)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public bool FindProduct()
+		{
+			IWebElement el = this.containerElement.FindElement(By.XPath("//td[@aria-describedby='list_Waste'][@title='Y']/preceding-sibling::td[@aria-describedby='list_Product']"), 2);
+			if (el != null)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public bool FindProductN()
+		{
+			IWebElement el = this.containerElement.FindElement(By.XPath("//td[@aria-describedby='list_Waste'][@title='N']/preceding-sibling::td[@aria-describedby='list_Product']"), 2);
+			if (el != null)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public bool ClickVendorSection()
+		{
+			IWebElement el = this.containerElement.FindElement(By.XPath("//span[text()='[SECT0770] Vendor Report']"), 2);
+			return el.TryClick();
+		}
+
+		public bool ClickASection()
+		{
+			IWebElement el = this.containerElement.FindElement(By.XPath("//span[text()='[TXALL]']"), 2);
+			return el.TryDoubleClick();
+		}
+
+		public bool CheckText()
+		{
+			IWebElement el = this.containerElement.FindElement(By.XPath("//td[contains(text(), 'There is no (or limited) data available for any components and waste code has been assigned as a conservative approach due to lack of significant data showing non-hazardous')]"), 2);
+			if (el != null)
+			{
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
