609ff4e5b2cd0ae7d4ea3a775568fd41eddd345a
Philip Nagel <philip.nagel@ul.com>
2021-02-12 09:20:35 -0500

    Removed Steps

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/New Product/Steps_NewProduct.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/New Product/Steps_NewProduct.cs
@@ -2761,22 +2761,6 @@ namespace UL.Selenium.Portal.WERCSmart.Steps.New_Product
 				"Failed to select: " + containerTypes[randomNumber], "Selected: " + containerTypes[randomNumber]);
 		}
 
-		[StepDefinition(@"I Select a height from the drop down list")]
-		public void GivenISelectAHeightFromTheDropDownList()
-		{
-			List<string> HeightList = new NewProduct().GetHeightOptions();
-			// the container type count must be greater than 1 or random number will throw argument out of range exception (cannot have a range between 1 and 0)
-			if (HeightList.Count <= 1)
-			{
-				Report.Failure("Expected > 1 options to appear under the height select");
-				return;
-			}
-			var random = new Random();
-			int randomNumber = random.Next(1, HeightList.Count - 1);
-			Report.IsTrue(new NewProduct().SelectHeight(HeightList[randomNumber]),
-				"Failed to select: " + HeightList[randomNumber], "Selected: " + HeightList[randomNumber]);
-		}
-
 		[StepDefinition(@"I enter Size Value: (.*)")]
 		public void GivenIEnterSizeValue(string size)
 		{
