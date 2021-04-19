769d4e15af962fceeecf3ec716aa5c77d4885930
Philip Nagel <philip.nagel@ul.com>
2021-04-19 15:34:47 -0400

    Editted Steps

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/Steps_Shared.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/Steps_Shared.cs
@@ -11218,15 +11218,6 @@ namespace UL.Selenium.Portal.WERCSmart.Steps
 			MyStepsNewProduct.GivenInTheNewProductPageIClickContinue("Additional Product Information");
 		}
 
-		[StepDefinition(@"I call shared step 144794 \(Login to WS as supplier with feed to Web viewers\)")]
-		public void GivenICallSharedStep144974LoginToWSAsSupplierWithFeedToWebViewers()
-		{
-			var selGlobalSteps = new GlobalSteps();
-
-			selGlobalSteps.GivenILogInWithEmailXAndPasswordY("purple-shape.kxxyxunf@mailosaur.io", "Thewercs3!");
-			new StepsHomepage().IfDataConsentRequestsModalIsShowingAddRequiredTiers();
-		}
-
 		[StepDefinition(@"I call shared step 86009 \(Retailer - PLP, Canada Only, Select Canadian Tire add PLP data - Continue\)")]
 		public void GivenICallSharedStepRetailer_PLPCanadaOnlySelectCanadianTireAddPLPData_Continue()
 		{
