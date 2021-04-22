429ecbb05df39e701a2bfd54b4d4ffc054723368
Philip Nagel <philip.nagel@ul.com>
2021-04-22 10:49:59 -0400

    Resolved Pull Request Conflicts

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Selenium Classes/ProductsGrid.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Selenium Classes/ProductsGrid.cs
@@ -1398,18 +1398,6 @@ namespace UL.Selenium.Portal.WERCSmart.Selenium_Classes
 			return productList.Count;
 		}
 
-		public bool InProductIDIngredientIDSKUFilterFieldSearchFollowingText(string text)
-		{
-			IWebElement field = this.containerElement.FindElement(By.XPath(".//input[@aria-describedby='internalProdIDAddOn']"), 2);
-			return field.TryEnterText(text);
-		}
-
-		public bool SelectSearchButtonNextToProductIDIngredientIDSKUFilterField()
-		{
-			IWebElement searchButton = this.containerElement.FindElement(By.XPath(".//input[@aria-describedby='internalProdIDAddOn']/following-sibling::span"), 2);
-			return searchButton.TryClick();
-		}
-
 	}
 
 	public class ProductGridItem : ProductsGrid
