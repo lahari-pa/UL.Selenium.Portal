4b8ff9a012868cb82205059ec13e5a86eb10653b
Philip Nagel <philip.nagel@ul.com>
2021-04-28 11:32:48 -0400

    Added Code

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/Steps_ProductGrid.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/Steps_ProductGrid.cs
@@ -867,6 +867,28 @@ namespace UL.Selenium.Portal.WERCSmart.Steps
 			Delay.Seconds(1);
 		}
 
+		[StepDefinition(@"I generate a random Product ID and save as: (.*)")]
+		public void GivenIGenerateARandomProductIDAndSaveAs(string savedAs)
+		{
+			string uPCNo = GeneralFunctions.GenerateUPCNumber();
+			Context.AddToContext(savedAs, uPCNo);
+			//Report.Info("Generated UPC No: " + uPCNo);
+			//Delay.Seconds(2);
+			Report.Info(uPCNo);
+			Delay.Seconds(1);
+		}
+
+		[StepDefinition(@"I generate a random Ingredient ID and save as: (.*)")]
+		public void GivenIGenerateARandomIngredientIDAndSaveAs(string savedAs)
+		{
+			string uPCNo = GeneralFunctions.GenerateUPCNumber();
+			Context.AddToContext(savedAs, uPCNo);
+			//Report.Info("Generated UPC No: " + uPCNo);
+			//Delay.Seconds(2);
+			Report.Info(uPCNo);
+			Delay.Seconds(1);
+		}
+
 		[StepDefinition(@"I generate (.*) random UPC numbers and save all to list named: (.*)")]
 		public void GivenIGenerateXRandomUPCNumbersAndSaveAs(int x, string savedAs)
 		{
