69986c4c9138a90029045b06b061d5992cd4c245
Andrew Glew <andrew.glew@ul.com>
2021-05-07 15:50:49 +0100

    minor

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/Steps_SHA.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/Steps_SHA.cs
@@ -1448,10 +1448,10 @@ namespace UL.Selenium.Portal.WERCSmart.Steps
 			Report.Screenshot();
 			Report.Info("Pressing Enter Key");
 			Report.Screenshot();
-			myStudioShaManager.PressEnterOnStatusFilter();			
-			myStudioShaManager.SelectFromStatusFilter(status);
-			Report.Info("Status Selected again");
-			Report.Screenshot();
+			//myStudioShaManager.PressEnterOnStatusFilter();			
+			//myStudioShaManager.SelectFromStatusFilter(status);
+			//Report.Info("Status Selected again");
+			//Report.Screenshot();
 
 
 			//This query is often very slow. Sometimes the results appear to have loaded but then several seconds later the
