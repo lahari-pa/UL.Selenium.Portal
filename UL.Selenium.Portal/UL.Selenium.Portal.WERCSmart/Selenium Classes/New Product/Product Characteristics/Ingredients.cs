01a3fa1dd0ca4c1231d945990193fb46db63d549
Andrew Glew <19095@global.ul.com>
2021-05-18 11:30:44 +0100

    Updated FIFRA list of ingredients

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Selenium Classes/New Product/Product Characteristics/Ingredients.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Selenium Classes/New Product/Product Characteristics/Ingredients.cs
@@ -1774,11 +1774,12 @@ namespace UL.Selenium.Portal.WERCSmart.Selenium_Classes.New_Product
 		public List<string> IngredientsFIFRAPopup()
 		{
 
-			//This acts as a hardcoded list of ingredient namest that cause a check for the Pesticide popup after pressing continue on the ingredients page
+			//This acts as a hardcoded list of ingredient names that cause a check for the Pesticide popup after pressing continue on the ingredients page
 			//Items in this list of currently consists of ingredients used in automation that currently reside in the SHA FIFRA or FIFRAR list of components.
 			//As it stands this list does not dynamically update to any changes to these component lists will need to manually input. 
 			List<string> ingredients = new List<string>();
 			ingredients.Add("Chlorine");
+			ingredients.Add("Chlorine dioxide");
 			ingredients.Add("Sodium hydroxide");
 
 
