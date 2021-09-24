5e933539d80fabffeb41374282dd5967c08d175a
Andrew Glew <19095@global.ul.com>
2021-09-24 16:15:02 +0100

    Reverted some upc changes

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/Steps_ProductGrid.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/Steps_ProductGrid.cs
@@ -770,7 +770,8 @@ namespace UL.Selenium.Portal.WERCSmart.Steps
 			for (int i = 0; i < 6; i++)
 			{
 				Report.Info("Generating upc attempt " + (i + 1));
-				string uPCNo = new UpcFunctions().GenerateUPC();
+				//string uPCNo = new UpcFunctions().GenerateUPC();
+				string uPCNo = GeneralFunctions.GenerateUPCNumber();
 				Report.Info("Generated UPC No: " + uPCNo);
 				Report.Info("Searching for generated upc number");
 				grid.UpcNumber = uPCNo;
@@ -811,8 +812,8 @@ namespace UL.Selenium.Portal.WERCSmart.Steps
 		[StepDefinition(@"I generate a random UPC number and save as: (.*)")]
 		public void GivenIGenerateARandomUPCNumberAndSaveAs(string savedAs)
 		{
-			string uPCNo = new UpcFunctions().GenerateUPC();
-			//string uPCNo = GeneralFunctions.GenerateUPCNumber();
+			//string uPCNo = new UpcFunctions().GenerateUPC();
+			string uPCNo = GeneralFunctions.GenerateUPCNumber();
 			Context.AddToContext(savedAs, uPCNo);
 			//Report.Info("Generated UPC No: " + uPCNo);
 			//Delay.Seconds(2);
@@ -824,7 +825,8 @@ namespace UL.Selenium.Portal.WERCSmart.Steps
 		[StepDefinition(@"Generate a random SKU number \(12 random digits\) and save as: (.*)")]
 		public void ThenGenerateARandomSKUNumberRandomDigitsAndSaveAsRandomSKU_(string savedAs)
 		{
-			string uPCNo = new UpcFunctions().GenerateUPC();
+			//string uPCNo = new UpcFunctions().GenerateUPC();
+			string uPCNo = GeneralFunctions.GenerateUPCNumber();
 			Context.AddToContext(savedAs, uPCNo);
 			//Report.Info("Generated UPC No: " + uPCNo);
 			//Delay.Seconds(2);
@@ -835,7 +837,8 @@ namespace UL.Selenium.Portal.WERCSmart.Steps
 		[StepDefinition(@"I generate a random Product ID and save as: (.*)")]
 		public void GivenIGenerateARandomProductIDAndSaveAs(string savedAs)
 		{
-			string uPCNo = new UpcFunctions().GenerateUPC();
+			//string uPCNo = new UpcFunctions().GenerateUPC();
+			string uPCNo = GeneralFunctions.GenerateUPCNumber();
 			Context.AddToContext(savedAs, uPCNo);
 			//Report.Info("Generated UPC No: " + uPCNo);
 			//Delay.Seconds(2);
@@ -846,7 +849,8 @@ namespace UL.Selenium.Portal.WERCSmart.Steps
 		[StepDefinition(@"I generate a random Ingredient ID and save as: (.*)")]
 		public void GivenIGenerateARandomIngredientIDAndSaveAs(string savedAs)
 		{
-			string uPCNo = new UpcFunctions().GenerateUPC();
+			//string uPCNo = new UpcFunctions().GenerateUPC();
+			string uPCNo = GeneralFunctions.GenerateUPCNumber();
 			Context.AddToContext(savedAs, uPCNo);
 			//Report.Info("Generated UPC No: " + uPCNo);
 			//Delay.Seconds(2);
@@ -2756,7 +2760,8 @@ namespace UL.Selenium.Portal.WERCSmart.Steps
 			int i = 1;
 			while (i <= numbersWanted)
 			{
-				string uPCNo = new UpcFunctions().GenerateUPC();
+				//string uPCNo = new UpcFunctions().GenerateUPC();
+				string uPCNo = GeneralFunctions.GenerateUPCNumber();
 				Context.AddToContext(savedAs + i, uPCNo);
 				Report.Info("Generated UPC No " + i + ": " + uPCNo);
 				Delay.Seconds(0.5);
