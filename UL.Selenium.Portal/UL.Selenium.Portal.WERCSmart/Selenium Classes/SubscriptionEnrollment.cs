0342aad9665c7317644c0723f1219046b53be85a
Andrew Glew <19095@global.ul.com>
2021-12-02 15:13:57 +0000

    Updated Support services xpath

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Selenium Classes/SubscriptionEnrollment.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Selenium Classes/SubscriptionEnrollment.cs
@@ -769,7 +769,7 @@ namespace UL.Selenium.Portal.WERCSmart.Selenium_Classes
 		public bool Bronze_click()
 		{
 			Report.Info("Attempting to Select Bronze Support Services Plan");
-			List<IWebElement> listofMenus = this.containerElement.FindElements(By.XPath($"//div[@class='col-sm-4']//label"), 2).ToList();
+			List<IWebElement> listofMenus = this.containerElement.FindElements(By.XPath($"//div[@class='col-md-12 nopadding']//label"), 2).ToList();
 			foreach (var menu in listofMenus)
 			{
 				string titleTrimmed = Regex.Replace(menu.Text, @"\r\n.*", "").Trim();
