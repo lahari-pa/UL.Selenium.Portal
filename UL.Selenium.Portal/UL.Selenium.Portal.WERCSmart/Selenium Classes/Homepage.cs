a2c0cace7aff0b44ebe0404b7c2254dc43cc1b06
Turkot, Olha <32143@global.ul.com>
2023-04-06 11:41:23 -0500

    Changed xpath

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Selenium Classes/Homepage.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Selenium Classes/Homepage.cs
@@ -261,7 +261,7 @@ namespace UL.Selenium.Portal.WERCSmart.Selenium_Classes
 
 		public bool AlertsButtonIsDisplayed(string button, string alert)
 		{
-			IWebElement AlertButton = SeleniumBrowser.WebBrowser.FindElement(By.XPath(".//div[@class='panel panel-default messaging']//p[contains(text(), '" + alert + "')]/../../following-sibling::td//a[text()='" + button + " ']"), 2);
+			IWebElement AlertButton = SeleniumBrowser.WebBrowser.FindElement(By.XPath(".//div[@class='panel panel-default messaging']//p[contains(text(), '" + alert + "')]/../../following-sibling::td//a[starts-with(text(), '" + button + "')]"), 2);
 			return AlertButton.Displayed;
 		}
 		//public bool DataConsentPopupPresent()
