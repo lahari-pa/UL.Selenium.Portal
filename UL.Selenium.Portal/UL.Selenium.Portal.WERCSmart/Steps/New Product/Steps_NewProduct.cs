7ef528c98efd55e2f111a58819abb93a85c24bb8
Shianne Ali <31850@global.ul.com>
2023-04-18 12:02:24 -0400

    changes to the subscription step

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/New Product/Steps_NewProduct.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/New Product/Steps_NewProduct.cs
@@ -2406,8 +2406,7 @@ namespace UL.Selenium.Portal.WERCSmart.Steps.New_Product
 			var mySub = new PaymentMethods_Subscription_Billing();
 			
 			if (mySub.Purchase_Header_Correct())
-			{
-			
+			{			
 				if (mySub.ConfirmOrderButtonExists())
 				{
 					Report.IsTrue(mySub.Confirm_Order_click(), "Failed to Click Confirm Order Button", "Confirm Order Button Clicked");
@@ -2415,12 +2414,22 @@ namespace UL.Selenium.Portal.WERCSmart.Steps.New_Product
 					GeneralUtilities.Wait_for_load_finish();
 					Report.Screenshot();
 				}
+				else
+				{
+					Report.Info("Purchase details page is not showing"); 
+				}
+
+				Report.Info("Checking if thank you page is showing instead");
 
 				var pmtk = new PaymentMethods_Thank_You();
 				if (pmtk.WaitForContainerToExist())
 				{					
 					Report.StartSubStep("In the Thank You screen I confirm the following statement is shown");
-					MyStepsPaymentMethods.ThenInTheThankYouScreenIConfirmTheFollowingStatementIsShownX("Thank you for registering your product on WERCSmart for assessment. The retailers may receive your assessment in approximately two (2) business days, if no delays in processing the assessment, and should no data issues arise.");					
+					MyStepsPaymentMethods.ThenInTheThankYouScreenIConfirmTheFollowingStatementIsShownX("Thank you for registering your product on WERCSmart for assessment. The retailers may receive your assessment in approximately two (2) business days, if no delays in processing the assessment, and should no data issues arise.");
+				}
+				else
+				{
+					Report.Info("Thank you page is not showing");				
 				}
 			}
 			else
@@ -2441,6 +2450,10 @@ namespace UL.Selenium.Portal.WERCSmart.Steps.New_Product
 					StepsSE_new.InSubscriprionEnrollmentModalClickButton("Checkout");
 					Report.StartSubStep("In the Payment Methods screen I click Continue");
 					MyStepsPaymentMethods.ThenIClickContinue();
+					Report.StartSubStep("In the Purchase Summary screen I click Confirm Order");
+					MyStepsPaymentMethods.ThenInThePurchaseSummaryScreenIClickConfirmOrder();
+					Report.StartSubStep("In the Thank You screen I check the Header is correct"); 
+					MyStepsPaymentMethods.ThenInTheThankYouScreenICheckTheHeaderIsCorrect();
 				}
 				else
 				{
