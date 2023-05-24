e2d778e262c6874dfeab92235b635fabb72692ad
Turkot, Olha <32143@global.ul.com>
2023-05-24 13:43:10 -0500

    Added step defenition

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/New Product/Steps_SelectRetailers.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/New Product/Steps_SelectRetailers.cs
@@ -338,5 +338,15 @@ namespace UL.Selenium.Portal.WERCSmart.Steps.New_Product
 			Report.IsTrue(retailerObject.ConfirmRetailerRegistrationCheckbox(value) == status, $"Failed to confirm the checkbox is {presentNotPresent}.", $"Successfully confirmed that the checkbox is {presentNotPresent}.");
 		}
 
+		[StepDefinition(@"I select the following retailers in the Select Retailers popup list view and check no more retailers can be selected:")]
+		public void ThenInTheWindowICheckOnlyOneRetailerCanBeSelected(Table retailers)
+		{
+			var selSelectRetailers = new SelectRetailers();
+
+			this.SelectRetailersInListView(retailers);
+			Report.IsTrue(selSelectRetailers.OnlyOneRetailerCanBeSelected(retailers), "Failed to verify no more retailers can be selected", "Succesfully verified no more retailers can be selected");
+
+		}
+
 	}
 }
