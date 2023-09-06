9b1f6603e8ef7c7aa7160ef1e6a49c5ebf6ca44d
schittampally <sai.chittampally@ul.com>
2023-09-06 12:54:36 -0400

    Removed the dupliacte method

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/New Product/Steps_NewProduct.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/New Product/Steps_NewProduct.cs
@@ -3594,30 +3594,6 @@ namespace UL.Selenium.Portal.WERCSmart.Steps.New_Product
 		{
 			Report.IsTrue(new NewProduct().ConfirmSKUFieldWasBlank(), "Failed to find the SKU field is blank", "Successfully found the SKU field is blank");
 		}
-		[StepDefinition(@"I (should|should not) see following container type from the drop down list")]
-		public void GivenIShouldSeeContainerTypeFromTheDropDownList(string shouldOrNot, Table table)
-		{
-			List<string> containerTypes = new NewProduct().GetContainerOptions();
-			if (shouldOrNot == "should")
-			{
-				foreach (TableRow Row in table.Rows)
-				{
-					Report.IsTrue(containerTypes.Contains(Row["Container Type"]), "Container type was not found", "Container Type was found on the upc page as expected");
-				}
-			}
-			else if (shouldOrNot == "should not")
-			{
-				foreach (TableRow Row in table.Rows)
-				{
-					Report.IsTrue(!containerTypes.Contains(Row["Container Type"]), "Container type was found", "Container Type was not found on the upc page as expected");
-				}
-			}
-			else
-			{
-				Report.Failure("input values must be either 'should' or 'should not'");
-			}
-		}
-
 	}
 
 	//public class UPCWarning : SeleniumBaseObject
