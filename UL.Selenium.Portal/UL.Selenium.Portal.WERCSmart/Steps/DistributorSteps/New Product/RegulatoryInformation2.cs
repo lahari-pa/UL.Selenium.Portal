d2b0b69421c4f3c013eaff2a651e2eb793dc09da
Turkot, Olha <32143@global.ul.com>
2023-09-28 14:52:30 -0500

    Added Regulatory Information 2 file with step

---
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/DistributorSteps/New Product/RegulatoryInformation2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using UL.Automation.WebDriver.Classes;
+using UL.Automation.Utilities.Functions;
+using UL.Automation.Reporting.Functions;
+using UL.Automation.SpecFlow.Classes;
+using TechTalk.SpecFlow;
+using TReVor.Api.Wrapper.Classes;
+using UL.Automation.Reporting;
+using UL.Automation.TReVor.Classes;
+using UL.Selenium.Portal.WERCSmart.Selenium_Classes;
+using UL.Selenium.Portal.WERCSmart.Selenium_Classes.New_Product;
+
+namespace UL.Selenium.Portal.WERCSmart.Steps.DistributorSteps.New_Product
+{
+	[Binding, Scope(Tag = "Product:WERCSmart_Account:Distributor_Page:NewProducts_Tab:ProductCharacteristics_Section:RegulatoryInformation2")]
+	internal class RegulatoryInformation2
+	{
+		[StepDefinition(@"In the New Product Section, set the radio option in section: 'Product contains microbeads': to: (Yes|No)")]
+		public void SelectProductTypeToCreate(string option)
+		{
+			string section = "Product contains microbeads";
+			new Steps_Prototype().SetRadioOptionInSectionTo(section, option);
+		}
+	}
+}
