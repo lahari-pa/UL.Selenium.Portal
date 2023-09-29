c4c6aafac0d99bdc568333dc21f2b67f1c547058
Shianne Ali <31850@global.ul.com>
2023-09-29 19:35:28 -0400

    added reg info 3 question step

---
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/New Product/RegulatoryInformation3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using UL.Automation.WebDriver.Classes;
+using UL.Automation.Utilities.Functions;
+using UL.Automation.Reporting.Functions;
+using UL.Automation.SpecFlow.Classes;
+using TechTalk.SpecFlow;
+using TReVor.Api.Wrapper.Classes;
+using UL.Automation.Reporting;
+using UL.Automation.TReVor.Classes;
+using UL.Selenium.Portal.WERCSmart.Classes;
+using UL.Selenium.Portal.WERCSmart.Selenium_Classes;
+using UL.Selenium.Portal.WERCSmart.Selenium_Classes.New_Product;
+
+namespace UL.Selenium.Portal.WERCSmart.Steps.SpecFlowSteps.New_Product
+{
+	[Binding, Scope(Tag = "RegulatoryInformation3")]
+	class RegulatoryInformation3
+	{
+		[StepDefinition(@"In the Regulatory Information 3 Section, set the checkbox option in section: 'Refer to your Product Label.  From the options, select those that appear on the Label.': to: (Drug Facts Panel|Supplement Facts Panel|Nutrition Facts Panel|None of the Above)")]
+		public void SelectProductTypeToCreate(string option1, string option2)
+		{
+			string section = "Refer to your Product Label.  From the options, select those that appear on the Label.";
+			new Steps_Prototype().SetTheSectionOptionTo(section, option1);
+		}
+	}
+}
