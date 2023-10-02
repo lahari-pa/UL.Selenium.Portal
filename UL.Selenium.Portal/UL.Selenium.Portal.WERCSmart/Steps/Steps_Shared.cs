1325bc8ad3307301a444b2ca0d9c64c1279faf21
schittampally <sai.chittampally@ul.com>
2023-10-02 09:42:57 -0400

    #Added missing '/' for shared step

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/Steps_Shared.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/Steps_Shared.cs
@@ -15831,7 +15831,7 @@ namespace UL.Selenium.Portal.WERCSmart.Steps
 			MyNewProduct.GivenInTheNewProductPageIClickContinue("Physical and Chemical Properties");
 		}
 		[StepDefinition(
-			@"I call Shared Step 209526(b) \(WPS Studio - PD\+ - set all data and publish using rule and doc queue for CKLT and MTR only\) for product saved as: (.*)")]
+			@"I call Shared Step 209526\(b\) \(WPS Studio - PD\+ - set all data and publish using rule and doc queue for CKLT and MTR only\) for product saved as: (.*)")]
 		public void GivenICallSharedStep209526WPSStudio_PD_SetAllDataAndPublish_CKLTAndMTROnly(string savedAs)
 		{
 			if (Context.Contains("ElectronicProduct"))
