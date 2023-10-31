252fbac16452746f8fea14f359ba79d9991b270b
Shianne Ali <31850@global.ul.com>
2023-10-31 16:16:50 -0400

    edits

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/New Product/RegulatoryInformation3.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/New Product/RegulatoryInformation3.cs
@@ -17,21 +17,25 @@ namespace UL.Selenium.Portal.WERCSmart.Steps.SpecFlowSteps.New_Product
 	[Binding, Scope(Tag = "RegulatoryInformation3")]
 	class RegulatoryInformation3
 	{
-		[StepDefinition(@"In the Regulatory Information 3 Section, set the checkbox option in section: 'Refer to your Product Label.  From the options, select those that appear on the Label.': to: (Drug Facts Panel|Supplement Facts Panel|Nutrition Facts Panel|None of the Above)")]
-		public void SelectProductLabel(string option1)
+		[StepDefinition(@"In the Regulatory Information 3 Section, in section: 'Refer to your Product Label.  From the options, select those that appear on the Label.' click the checkbox option: (Drug Facts Panel|Supplement Facts Panel|Nutrition Facts Panel|None of the Above)")]
+		public void SelectProductLabel(string option)
 		{
 			string section = "Refer to your Product Label.  From the options, select those that appear on the Label.";
-			new Steps_Prototype().SetTheSectionOptionTo(section, option1);
+			new Steps_Prototype().SetTheSectionOptionTo(section, option);
 		}
 
 
-		[StepDefinition(@"In the Regulatory Information 3 Section, the link: (OTC Drug Facts Label may include Active Ingredient|Nutritional and Supplement Labels|Dietary Supplements Label) (should|should not) be displayed")]
+		[StepDefinition(@"In the Regulatory Information 3 Section, the following link: (OTC Drug Facts Label \(may include Active Ingredient\)|Nutritional and Supplement Labels|Dietary Supplements Label) (should|should not) be displayed")]
 		public void Regulatory3LinkExists(string linkText, string condition)
 		{
 			new Steps_Prototype().LinkElementExists(condition, linkText);
 		}
 
-
+		[StepDefinition(@"In the Regulatory Information 3 Section, click the following link: (OTC Drug Facts Label \(may include Active Ingredient\)|Nutritional and Supplement Labels|Dietary Supplements Label)")]
+		public void Regulatory3ClickLinks(string linkText)
+		{
+			new Steps_Prototype().ClickLinkElement(linkText);
+		}
 
 
 
