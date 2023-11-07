e7cd28b8475dec1a88424dc006ecd9b98760382d
Turkot, Olha <32143@global.ul.com>
2023-11-07 13:14:39 -0600

    Added new step

---
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/New Product/ProductIncludesBattery.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace UL.Selenium.Portal.WERCSmart.Steps.SpecFlowSteps.New_Product
+{
+	[Binding, Scope(Tag = "Product:WERCSmart_Page:NewProducts_Tab:ProductType_Section:ProductIncludesBattery")]
+	class WERCSmart_Distributor_NewProducts_ProductType_NewProduct
+	{
+		[StepDefinition(@"In the Product Includes Battery Section, set the radio option in section: 'Indicate how battery is packaged': to: (Installed in the product|The battery is shipped with but not included in my product.|Installed in the Product and Shipped with Additional Batteries not within the Product.)")]
+		public void SelectIndicateHowBatteryIsPackaged(string option)
+		{
+			string section = "Indicate how battery is packaged";
+			new Steps_Prototype().SetRadioOptionInSectionTo(section, option);
+		}
+		[StepDefinition(@"In the Product Includes Battery Section, set the radio option in section: 'Battery Type': to: (Carbon Zinc|Alkaline)")]
+		public void SelectBatteryType(string option)
+		{
+			string section = "Battery Type";
+			new Steps_Prototype().SetTheSectionOptionTo(section, option);
+		}
+	}
+}
