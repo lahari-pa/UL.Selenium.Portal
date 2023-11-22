6b223c194fafbe04c7eca7e5e66c9659ddd06881
Turkot, Olha <32143@global.ul.com>
2023-11-22 14:53:49 -0600

    Created file

---
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/New Product/PesticideDetailsUS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace UL.Selenium.Portal.WERCSmart.Steps.SpecFlowSteps.New_Product
+{
+	[Binding, Scope(Tag = "Product:WERCSmart_Page:NewProducts_Tab:ProductCharacteristics_Section:PesticideDetailsUS")]
+	class WERCSmart_Distributor_NewProducts_PesticideDetailsUS
+	{
+	}
+}
