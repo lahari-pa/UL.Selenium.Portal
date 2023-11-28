abad6871a333a424cb0d8fc70686ddcd86062181
Shianne Ali <31850@global.ul.com>
2023-11-28 18:29:35 -0500

    added steps

---
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/New Product/Product Type/PhysicalAndChemicalProp.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace UL.Selenium.Portal.WERCSmart.Steps.SpecFlowSteps.New_Product.Product_Type
+{
+	[Binding, Scope(Tag = "PhysicalAndChemicalProp")]
+	internal class PhysicalAndChemicalProp
+	{
+		// ADD OPT. FOR gas cylinder text = "Product is packaged in a gas cylinder (e.g.- whip cream)" -- escape issue 
+
+		[StepDefinition(@"In the Physical and Chemical Properties Section, set the option in section: 'Primary Physical State' to: (Liquid|Solid|Gas|Aerosol)")]
+		public void SelectPrimaryPhsicalState(string option)
+		{
+			string section = "Primary Physical State";
+			new Steps_Prototype().SetTheSectionOptionTo(section, option);
+		}
+
+		// INSTERT SECONDARY PHYSICAL STATE STEP HERE -- dropdown 
+
+
+		[StepDefinition(@"In the Physical and Chemical Properties Section, for section: 'Relative Density' enter text: (.*)")]
+		public void EnterTextForRelativeDensity(string text)
+		{
+			string section = "Relative Density";
+			new Steps_Prototype().SetTheSectionOptionTo(section,text);
+		}
+
+		//INSTERT RELATIVE DENSITY SET OPTION STEP HERE -- escape issue 
+
+		[StepDefinition(@"In the Physical and Chemical Properties Section, for sub section: 'pH' enter text: (.*)")]
+		public void EnterTextForPH(string text)
+		{
+			string section = "pH";
+			new Steps_Prototype().SetTheSectionOptionTo(section, text);
+		}
+
+		[StepDefinition(@"In the Physical and Chemical Properties Section, for sub section: 'pH' select the checkbox option: 'I do not have exact pH data available to me'")]
+		public void SelectCheckboxForPH(string text)
+		{
+			string section = "pH";
+			new Steps_Prototype().SetTheSectionOptionTo(section, text);
+		}
+
+		//INSTERT PH STEP HERE --dropdown
+
+		[StepDefinition(@"In the Physical and Chemical Properties Section, for question: 'When mixed with an equal amount of water- will this produce a solution with a pH <= 2 or a pH >= 12.5?' set the option to: (Yes|No)")]
+		public void SelectProductContainsMicrobeads(string option)
+		{
+			string section = "When mixed with an equal amount of water- will this produce a solution with a pH <= 2 or a pH >= 12.5?";
+			new Steps_Prototype().SetTheSectionOptionTo(section, option);
+		}
+
+		// next step -- boiling point
+
+
+	}
+}
