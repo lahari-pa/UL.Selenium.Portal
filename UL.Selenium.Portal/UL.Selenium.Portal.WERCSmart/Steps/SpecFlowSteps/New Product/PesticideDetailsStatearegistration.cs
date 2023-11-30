b641d1b1c0b967712167b4d51d3d45e3360871ee
Turkot, Olha <32143@global.ul.com>
2023-11-30 09:44:23 -0600

    Added new step

---
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/New Product/PesticideDetailsStatearegistration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace UL.Selenium.Portal.WERCSmart.Steps.SpecFlowSteps.New_Product
+{
+	[Binding, Scope(Tag = "Product:WERCSmart_Page:NewProducts_Tab:ProductCharacteristics_Section:PesticideDetailsStatearegistration")]
+	class WERCSmart_Distributor_NewProducts_ProductCharacteristics_PesticideDetailsStatearegistration
+	{
+		internal class PesticideDetailsStatearegistration
+		{
+
+
+		}
+	}
+}
