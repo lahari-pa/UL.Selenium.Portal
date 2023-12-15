b998f90ec010f382e866a6663efa2105e94cfd7b
Turkot, Olha <32143@global.ul.com>
2023-12-15 10:59:14 -0600

    Added page file

---
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/New Product/Product Characteristics/VOCForCaliforniaAirDistrict.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace UL.Selenium.Portal.WERCSmart.Steps.SpecFlowSteps.New_Product.Product_Characteristics
+{
+	[Binding, Scope(Tag = "Product:WERCSmart_Page:NewProducts_Tab:ProductCharacteristics_Section:VOCForCaliforniaAirDistrict")]
+	class WERCSmart_Distributor_NewProducts_VOCForCaliforniaAirDistrict
+	{
+		[StepDefinition(@"In the VOC for California Air District\(s\) Section, enter the value in section: 'VOC info' for Canada: (.*)")]
+		public void SEnterVOCInfo(string option)
+		{
+			string section = "Canada";
+			new Steps_Prototype().SetTheSectionOptionTo(section, option);
+		}
+	}
+}
