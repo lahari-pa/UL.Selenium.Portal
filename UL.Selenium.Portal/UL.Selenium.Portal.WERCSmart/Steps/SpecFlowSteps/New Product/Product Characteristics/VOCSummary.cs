7bd864feff198326a0286d4005b19bba6d8f9adf
Turkot, Olha <32143@global.ul.com>
2024-01-03 15:19:00 -0600

    Added new steps

---
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/New Product/Product Characteristics/VOCSummary.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace UL.Selenium.Portal.WERCSmart.Steps.SpecFlowSteps.New_Product.Product_Characteristics
+{
+
+	[Binding, Scope(Tag = "Product:WERCSmart_Page:NewProducts_Tab:ProductCharacteristics_Section:VOC_Summary")]
+	class WERCSmart_Distributor_NewProducts_VOCSummary
+	{
+		[StepDefinition(@"In the Volatile Organic Compound Summary Section, confirm that I see the following 'CARB' value: (.*)")]
+		public void CARBValue(string value)
+		{
+			string section = "CARB";
+			new Steps_Prototype().ThenIConfirmThatISeeTheFollowingCARBValue(section, value);
+		}
+		[StepDefinition(@"In the Volatile Organic Compound Summary Section, confirm that I see the following 'MVOC' value: (.*)")]
+		public void MVOCValue(string value)
+		{
+			string section = "MVOC";
+			new Steps_Prototype().ThenIConfirmThatISeeTheFollowingCARBValue(section, value);
+		}
+		[StepDefinition(@"In the Volatile Organic Compound Summary Section, confirm that I see the following 'HVOC' value: (.*)")]
+		public void HVOCValue(string value)
+		{
+			string section = "HVOC";
+			new Steps_Prototype().ThenIConfirmThatISeeTheFollowingCARBValue(section, value);
+		}
+		[StepDefinition(@"In the Volatile Organic Compound Summary Section, confirm that I see the following 'VOC Grams Ozone' value: (.*)")]
+		public void VOCGramsOzoneValue(string value)
+		{
+			string section = "VOC Grams Ozone";
+			new Steps_Prototype().ThenIConfirmThatISeeTheFollowingCARBValue(section, value);
+		}
+		[StepDefinition(@"In the Volatile Organic Compound Summary Section, confirm that I see the following 'OTC Model Rule' value: (.*)")]
+		public void OTCModelRuleValue(string value)
+		{
+			string section = "OTC Model Rule";
+			new Steps_Prototype().ThenIConfirmThatISeeTheFollowingCARBValue(section, value);
+		}
+		[StepDefinition(@"In the Volatile Organic Compound Summary Section, confirm that I see todays 'VOC Analysis Date'")]
+		public void VOCAnalsisDate()
+		{
+			new Steps_Prototype().ThenIConfirmThatISeeTodaysVOCAnalysisDate();
+		}
+		[StepDefinition(@"In the Volatile Organic Compound Summary Section, confirm that I see the following 'VOC content in g/L, including water and exempt compounds' value: (.*)")]
+		public void VOCContentingLIncludingWater(string value)
+		{
+			string section = "VOC content in g/L, including water and exempt compounds";
+			new Steps_Prototype().ThenIConfirmThatISeeTheFollowingCARBValue(section, value);
+		}
+		[StepDefinition(@"In the Volatile Organic Compound Summary Section, confirm that I see the following 'VOC content in g/L' value: (.*)")]
+		public void VOCContentingL(string value)
+		{
+			string section = "VOC content in g/L";
+			new Steps_Prototype().ThenIConfirmThatISeeTheFollowingCARBValue(section, value);
+		}
+			[StepDefinition(@"In the Volatile Organic Compound Summary Section, confirm that I see the following 'VOC percent as sold' value: (.*)")]
+		public void VOCPercentAsSold(string value)
+		{
+			string section = "VOC percent as sold";
+			new Steps_Prototype().ThenIConfirmThatISeeTheFollowingCARBValue(section, value);
+		}
+		[StepDefinition(@"In the Volatile Organic Compound Summary Section, confirm that I see the following 'VOC percent diluted for use' value: (.*)")]
+		public void VOCPercentDilutedForUse(string value)
+		{
+			string section = "VOC percent diluted for use";
+			new Steps_Prototype().ThenIConfirmThatISeeTheFollowingCARBValue(section, value);
+		}
+		[StepDefinition(@"In the Volatile Organic Compound Summary Section, for 'Your acknowledgement of this registration includes that your product..' set 'Yes, I Acknowledge'")]
+		public void SetYesIAcknowledge()
+		{
+			string section = "Your acknowledgement of this registration includes that your product";
+			new Steps_Prototype().SetTheSectionOptionTo(section, "Yes, I Acknowledge");
+		}
+
+	}	
+}
--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/Steps_Prototype.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/Steps_Prototype.cs
@@ -527,5 +527,28 @@ namespace UL.Selenium.Portal.WERCSmart.Steps
 		{
 			Report.IsTrue(new PesticideDetailsStateRegistrationTable().ClickSelectAllForStatus(status), $"Failed to click 'Select All' for {status}", $"Successfully clicked 'Select All' for status {status}");
 		}
+
+		[StepDefinition(@"I confirm that I see the following (.*) value: (.*)")]
+		public void ThenIConfirmThatISeeTheFollowingCARBValue(string category, string expectedValue)
+		{
+			var newProductpage = new NewProduct();
+			string foundValue = newProductpage.GetValueVOCSummary(category);
+			Report.IsTrue(foundValue?.Trim() == expectedValue.Trim(),
+				"value was not as expected! Expected: " + expectedValue + ", but found: " + foundValue + "!",
+				"value was showing: " + expectedValue + ", as expected!");
+		}
+
+		[StepDefinition(@"I confirm that I see todays VOC Analysis Date")]
+		public void ThenIConfirmThatISeeTodaysVOCAnalysisDate()
+		{
+			string date = DateTime.Now.ToString("MM/dd/yyyy");
+			var newProductpage = new NewProduct();
+			string found = newProductpage.GetValueVOCSummary("VOC Analysis");
+
+			Report.IsTrue(found.Trim() == date.Trim(),
+				"date was not as expected! Expected: " + date + ", but found: " + found + "!",
+				"statement was showing: " + date + ", as expected!");
+		}
+
 	}
 }
