415a7bfd8d6ef82c9a301ac6e8f44e3dcac37321
Turkot, Olha <32143@global.ul.com>
2024-01-16 13:25:24 -0600

    Added steps

---
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/New Product/Product Characteristics/USDepartamentOfTransportationDOT.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace UL.Selenium.Portal.WERCSmart.Steps.SpecFlowSteps.New_Product.Product_Characteristics
+{
+	[Binding, Scope(Tag = "Product:WERCSmart_Page:NewProducts_Tab:ProductCharacteristics_Section:USDepartamentOfTransportationDOT")]
+	class WERCSmart_Distributor_NewProducts_USDepartamentOfTransportationDOT
+	{
+		[StepDefinition(@"In the U. S. Department of Transportation \(DOT\) Classification Section, set the option in section: 'UN Number': to: (.*)")]
+		public void SetUNNumberIMDG(string option)
+		{
+			string section = "UN Number";
+			new Steps_Prototype().SetTheSectionOptionTo(section, option);
+		}
+		[StepDefinition(@"In the U. S. Department of Transportation \(DOT\) Classification Section, set the option in section: 'Proper Shipping Name': to: (.*)")]
+		public void SetProperShippingNameIMDG(string option)
+		{
+			string section = "Proper Shipping Name";
+			new Steps_Prototype().SetTheSectionOptionTo(section, option);
+		}
+		[StepDefinition(@"In the U. S. Department of Transportation \(DOT\) Classification Section, set the option in section: 'Technical Name \(if applicable\)': to: (.*)")]
+		public void SetTechnicalNameIMDG(string option)
+		{
+			string section = "Technical Name (if applicable)";
+			new Steps_Prototype().SetTheSectionOptionTo(section, option);
+		}
+		[StepDefinition(@"In the U. S. Department of Transportation \(DOT\) Classification Section, set the option in section: 'Hazard Class': to: (.*)")]
+		public void SetHazardClassIMDG(string option)
+		{
+			string section = "Hazard Class (select)";
+			new Steps_Prototype().SetTheSectionOptionTo(section, option);
+		}
+		[StepDefinition(@"In the U. S. Department of Transportation \(DOT\) Classification Section, set the option in section: 'Packing Group': to: (.*)")]
+		public void SetPackingGroupIMDG(string option)
+		{
+			string section = "Packing Group (select)";
+			new Steps_Prototype().SetTheSectionOptionTo(section, option);
+		}
+		[StepDefinition(@"In the U. S. Department of Transportation \(DOT\) Classification Section, set the option in section: 'Special Provision Number': to: (.*)")]
+		public void SetSpecialProvisionNumber(string option)
+		{
+			string section = "Special Provision Number";
+			new Steps_Prototype().SetTheSectionOptionTo(section, option);
+		}
+		[StepDefinition(@"In the U. S. Department of Transportation \(DOT\) Classification Section, set the option in section: 'Regulatory reference for any exemption or exceptions taken': to: (.*)")]
+		public void SetRegulatoryReferenceForAnyExemptionOrExceptionsTaken(string option)
+		{
+			string section = "Regulatory reference for any exemption or exceptions taken";
+			new Steps_Prototype().SetTheSectionOptionTo(section, option);
+		}
+		[StepDefinition(@"In the U. S. Department of Transportation \(DOT\) Classification Section, set the option in section: 'Product has a boiling point of <=35⁰C and flash point of >60⁰C. Packing Group selected is not consistent with this data. Verify the data and transportation packing group. If problem persists, please contact Support.': to: (Based on defined viscosity parameters, this product is classified as PG III.|The Packaging Group inconsistency is allowed based on a Special Provision associated with the assigned UN#.|The Packaging Group inconsistency is allowed based on an exemption or exception associated with the assigned UN#.|The UN# classification assigned to this product has a specific Packaging Group required.)")]
+		public void SetProductHasBoilingPoint(string option)
+		{
+			string section = "Product has a boiling point of <=35⁰C and flash point of >60⁰C. Packing Group selected is not consistent with this data.  Verify the data and transportation packing group.  If problem persists, please contact Support.";
+			new Steps_Prototype().SetRadioOptionInSectionTo(section, option);
+		}
+		/*
+		[StepDefinition(@"In the U. S. Department of Transportation \(DOT\) Classification Section, verify section: 'UN Number' contains value: (.*)")]
+		public void VerifyUNNumberIMDG(string value)
+		{
+			string section = "UN Number";
+			new Steps_Prototype().CheckingFieldInputIsCorrect(section, value);
+		}
+		[StepDefinition(@"In the U. S. Department of Transportation \(DOT\) Classification Section, verify section: 'Proper Shipping Name' contains value: (.*)")]
+		public void VerifyProperShippingNameIMDG(string value)
+		{
+			string section = "Proper Shipping Name";
+			new Steps_Prototype().CheckingFieldInputIsCorrect(section, value);
+		}
+		[StepDefinition(@"In the U. S. Department of Transportation \(DOT\) Classification Section, verify section: 'Technical Name \(if applicable\)' contains value: (.*)")]
+		public void VerifyTechnicalNameIMDG(string value)
+		{
+			string section = "Technical Name (if applicable)";
+			new Steps_Prototype().CheckingFieldInputIsCorrect(section, value);
+		}
+		[StepDefinition(@"In the International Marine \(IMDG\) Classification Section, verify section: 'Hazard Class \(select\)' contains value: (.*)")]
+		public void VerifyHazardClassIMDG(string value)
+		{
+			string section = "Hazard Class (select)";
+			new Steps_Prototype().CheckingFieldInputIsCorrect(section, value);
+		}
+		[StepDefinition(@"In the U. S. Department of Transportation \(DOT\) Classification Section, verify section: 'Packing Group \(select\)' contains value: (.*)")]
+		public void VerifyPackingGroupIMDG(string value)
+		{
+			string section = "Packing Group (select)";
+			new Steps_Prototype().CheckingFieldInputIsCorrect(section, value);
+		}*/
+	}
+	
+}
