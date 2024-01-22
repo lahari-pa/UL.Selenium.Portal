e626726a1e57d4c1dd1dd3e0a4cfcc0dbc942ced
Turkot, Olha <32143@global.ul.com>
2024-01-22 15:17:26 -0600

    Added steps

---
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/New Product/Product Characteristics/USDOTClassification.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UL.Selenium.Portal.WERCSmart.Steps.SpecFlowSteps.New_Product.Product_Characteristics
+{
+	internal class USDOTClassification
+	{
+	}
+}
