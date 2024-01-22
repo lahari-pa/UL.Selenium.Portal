18f9c2d1953e46d94e3732fff575588d13f43172
Turkot, Olha <32143@global.ul.com>
2024-01-22 14:52:13 -0600

    Added step

---
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Selenium Classes/New Product/Product Characteristics/CreateTheKit.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UL.Automation.WebDriver.BaseClasses;
+using UL.Automation.WebDriver.Extensions;
+
+namespace UL.Selenium.Portal.WERCSmart.Selenium_Classes.New_Product.Product_Characteristics
+{
+	public class CreateTheKit : SeleniumBaseObject
+	{
+		protected override By ContainerElementLocator => By.XPath("//div[@class='col-md-12 rpds-grid']");
+		private IWebElement SearchInput => this.ContainerElement.FindElement(By.XPath($".//span[@class='selection']"), 2);
+
+		public bool SearchInputExists()
+		{
+			return this.SearchInput != null;
+		}
+		public bool SearchInputClick()
+		{
+			return this.SearchInput.TryClick();
+		}
+	
+	}
+}
