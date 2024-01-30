f83c19cf06eeacb65c9f8ed89f0f4ff8f35e739e
Shianne Ali <31850@global.ul.com>
2024-01-30 10:28:41 -0500

    added new steps

---
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Selenium Classes/New Product/Review and Submit/AdditionalDocsContactInfo.cs
@@ -0,0 +1,130 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UL.Automation.Reporting.Functions;
+using UL.Automation.WebDriver.BaseClasses;
+using UL.Automation.WebDriver.Extensions;
+
+namespace UL.Selenium.Portal.WERCSmart.Selenium_Classes.New_Product
+{
+	public class AdditionalDocsContactInfo : SeleniumBaseObject
+	{
+
+		protected override By ContainerElementLocator => By.XPath("//div[@class='panel panel-default panel-table']");
+
+		private IWebElement SubFormatTypeDD => this.ContainerElement.FindElement(By.XPath(".//select[@class='form-control']"), 2);
+
+		private IWebElement Address1 => this.ContainerElement.FindElement(By.XPath(".//td[2]//input[@class='form-control']"), 2);
+		private IWebElement Address2 => this.ContainerElement.FindElement(By.XPath(".//td[3]//input[@class='form-control']"), 2);
+		private IWebElement Phone => this.ContainerElement.FindElement(By.XPath(".//td[4]//input[@class='form-control']"), 2);
+		private IWebElement EmergencyPhone => this.ContainerElement.FindElement(By.XPath(".//td[5]//input[@class='form-control']"), 2);
+		private IWebElement Email => this.ContainerElement.FindElement(By.XPath(".//td[4]//input[@class='form-control']"), 2);
+		private IWebElement RemoveButton => this.ContainerElement.FindElement(By.XPath(".//td[@class='remove delete-row col-xs-1']"), 2);
+		private IWebElement AddRowButton => this.ContainerElement.FindElement(By.XPath(".//td[@class='col-xs-12']//button[@class='btn btn-primary']"), 2); 
+
+		public bool SubFormatTypeDropDownExists()
+		{
+			Report.Info("Checking to see if the Sub Format Type dropdown exists"); 
+			return this.SubFormatTypeDD != null;
+		}
+		
+		 public bool SelectSubFormatType(string value)
+		{
+			try
+			{
+				Report.Info("Selecting text from the Sub Format Type Drown down");
+				this.SubFormatTypeDD.Select(value);
+				return true;
+			}
+			catch (Exception)
+			{
+				Report.Info("Could not select an option from the Sub Format Type drop drop"); 
+				return false;
+			}
+		}
+
+		public bool EnterTextAddress1(string text)
+		{
+			if(this.Address1 != null)
+			{
+				Report.Info("Attempting to enter text in the first Address textbox");
+				return this.Address1.TryEnterText(text); 
+			}
+			Report.Info("Could not find the first address textbox");
+			return false;
+		}
+
+		public bool EnterTextAddress2(string text)
+		{
+			if (this.Address2 != null)
+			{
+				Report.Info("Attempting to enter text in the second Address textbox");
+				return this.Address2.TryEnterText(text);
+			}
+			Report.Info("Could not find the second address textbox");
+			return false;
+		}
+
+		public bool EnterTextPhone(string text)
+		{
+			if (this.Phone != null)
+			{
+				Report.Info("Attempting to enter text in the Phone textbox");
+				return this.Phone.TryEnterText(text);
+			}
+			Report.Info("Could not find the phone textbox");
+			return false;
+		}
+
+
+		public bool EnterTextEmergencyPhone(string text)
+		{
+			if (this.EmergencyPhone != null)
+			{
+				Report.Info("Attempting to enter text in the Emergency Phone textbox");
+				return this.EmergencyPhone.TryEnterText(text);
+			}
+			Report.Info("Could not find the Emergency Phone textbox");
+			return false;
+		}
+
+
+		public bool EnterTextEmail(string text)
+		{
+			if (this.Email != null)
+			{
+				Report.Info("Attempting to enter text in the Email textbox");
+				return this.Email.TryEnterText(text);
+			}
+			Report.Info("Could not find the Email textbox");
+			return false;
+		}
+
+		public bool ClickRemoveButton()
+		{
+			if (this.RemoveButton == null)
+			{
+				Report.Info("Cound not find the remove button");
+				return false;
+			}
+			return this.RemoveButton.TryClick();
+		}
+
+		public bool ClickAddRowButton()
+		{
+			if (this.AddRowButton == null)
+			{
+				Report.Info("Cound not find the Add Row button");
+				return false;
+			}
+			return this.AddRowButton.TryClick();
+		}
+
+
+
+	}
+
+}
---
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/New Product/Review and Submit/AdditionalDocsContactInfo_Steps.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using UL.Selenium.Portal.WERCSmart.Selenium_Classes.New_Product;
+
+namespace UL.Selenium.Portal.WERCSmart.Steps.SpecFlowSteps.New_Product.Review_and_Submit
+{
+	[Binding, Scope(Tag = "AdditionalDocsContactInfo")]
+	public class AdditionalDocsContactInfo_Steps
+	{
+
+		[StepDefinition(@"In the Additional Documents -> Contact Information section, for section: 'Manufacturer Name' enter text: (.*)")]
+		public void EnterTextForManfacturerName(string text)
+		{
+			string section = "Manufacturer Name";
+			new Steps_Prototype().SetTheSectionOptionTo(section, text);
+		}
+
+		[StepDefinition(@"In the Additional Documents -> Contact Information section, for section: 'Address' enter text: (.*)")]
+		public void EnterTextForAddress(string text)
+		{
+			string section = "Address";
+			new Steps_Prototype().SetTheSectionOptionTo(section, text);
+		}
+
+		[StepDefinition(@"In the Additional Documents -> Contact Information section, for section: 'Phone' enter text: (.*)")]
+		public void EnterTextForPhone(string text)
+		{
+			string section = "Phone";
+			new Steps_Prototype().SetTheSectionOptionTo(section, text);
+		}
+
+		[StepDefinition(@"In the Additional Documents -> Contact Information section, for section: 'Emergency Phone' enter text: (.*)")]
+		public void EnterTextForEmergencyPhone(string text)
+		{
+			string section = "Emergency Phone";
+			new Steps_Prototype().SetTheSectionOptionTo(section, text);
+		}
+
+		[StepDefinition(@"In the Additional Documents Contact Information section, in the sub section: 'Please provide contact information required to display on SDS\(s\) per region.' set the option in the section: 'Sub Format type' to : (Mexico GHS SDS|OSHA GHS SDS|Brazil GHS SDS|China GHS SDS|" +
+		"Vietnam  GHS SDS|Eco Logo|European GHS SDS|Canada GHS SDS|Indonesia  GHS SDS|Japan GHS SDS|Korea GHS SDS|North American Combined GHS SDS|Product Development Regulatory Report|Singapore GHS SDS|TCLP Calculation|Transportation Classification|"+
+		"Thailand GHS SDS|Taiwan GHS SDS|Australia GHS SDS|Assessment Summary Report|VOC Restriction|Malaysia GHS SDS)")]
+		public void EnterTextForSubFormatType(string option)
+		{
+			
+			new AdditionalDocsContactInfo().SelectSubFormatType(option);
+		}
+
+		[StepDefinition(@"In the Additional Documents  Contact Information section, in the sub section: 'Please provide contact information required to display on SDS\(s\) per region.' enter text for the first 'Address': (.*)")]
+		public void EnterTextForSubSectionAddress1(string text)
+		{
+			new AdditionalDocsContactInfo().EnterTextAddress1(text);
+		}
+
+		[StepDefinition(@"In the Additional Documents Contact Information section, in the sub section: 'Please provide contact information required to display on SDS\(s\) per region.' enter text for the second 'Address': (.*)")]
+		public void EnterTextForSubSectionAddress2(string text)
+		{
+			new AdditionalDocsContactInfo().EnterTextAddress2(text);
+		}
+
+		[StepDefinition(@"In the Additional Documents Contact Information section, in the sub section: 'Please provide contact information required to display on SDS\(s\) per region.' enter text for 'Phone' : (.*)")]
+		public void EnterTextForSubSectionPhone(string text)
+		{
+			new AdditionalDocsContactInfo().EnterTextPhone(text);
+		}
+
+		[StepDefinition(@"In the Additional Documents Contact Information section, in the sub section: 'Please provide contact information required to display on SDS\(s\) per region.' enter text for 'Emergency Phone' : (.*)")]
+		public void EnterTextForSubSectionEmergencyPhone(string text)
+		{
+			new AdditionalDocsContactInfo().EnterTextEmergencyPhone(text);
+		}
+
+		[StepDefinition(@"In the Additional Documents Contact Information section, in the sub section: 'Please provide contact information required to display on SDS\(s\) per region.' enter text for 'Email' : (.*)")]
+		public void EnterTextForSubSectionEmail(string text)
+		{
+			new AdditionalDocsContactInfo().EnterTextEmail(text);
+		}
+
+
+		[StepDefinition(@"In the Additional Documents -> Contact Information section, in the sub section: 'Please provide contact information required to display on SDS(s) per region.' click the 'Remove' button")]
+		public void ClickRemoveButton()
+		{
+			new AdditionalDocsContactInfo().ClickRemoveButton();
+		}
+
+		[StepDefinition(@"In the Additional Documents -> Contact Information section, in the sub section: 'Please provide contact information required to display on SDS(s) per region.' click the 'Add Row' button")]
+		public void ClickAddRowButton()
+		{
+			new AdditionalDocsContactInfo().ClickAddRowButton();
+		}
+
+
+		[StepDefinition(@"In the Additional Documents -> Contact Information section, in the sub section: 'Please go to My Account to manage your SDS emergency contact information for this document format.' click the 'My Account' link")]
+		public void ClicMyAccountLink()
+
+		{
+			string linkText = "My Account";
+			new Steps_Prototype().ClickLinkElement(linkText);
+		}
+
+	}
+}
