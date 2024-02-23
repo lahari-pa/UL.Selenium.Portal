99ddb1a419d17fbb3d3f06fdfb38555d4e8842b2
Turkot, Olha <32143@global.ul.com>
2024-02-23 08:32:28 -0600

    Added DataAcceptance file

---
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/New Product/Review and Submit/DataAcceptance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace UL.Selenium.Portal.WERCSmart.Steps.SpecFlowSteps.New_Product.Review_and_Submit
+{
+	[Binding, Scope(Tag = "Product:WERCSmart_Page:NewProducts_Tab:ReviewAndSubmit_Section:DataAcceptance")]
+	class WERCSmart_Distributor_NewProducts_ReviewAndSubmit_DataAcceptance
+	{
+		[StepDefinition(@"In the Data Acceptance Section, (check|uncheck) 'Agreed' checkbox")]
+		public void CheckUncheckAgreedCheckbox(string checked_unchecked)
+		{
+			string checkbox = "Agreed";
+			new Steps_Prototype().ICheckTheCheckboxWithDescription(checked_unchecked, checkbox);
+		}
+		[StepDefinition(@"In the Data Acceptance Section, click 'Summary' button")]
+		public void ClickSummaryButton()
+		{
+			string button = "Summary";
+			new Steps_Prototype().ClickButton(button);
+		}
+	}
+}
