88f6a6e71c5466bc459a2d23876d75cda591fe08
Turkot, Olha <32143@global.ul.com>
2024-02-23 12:50:06 -0600

    Added Summary file

---
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/New Product/Review and Submit/Summary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace UL.Selenium.Portal.WERCSmart.Steps.SpecFlowSteps.New_Product.Review_and_Submit
+{
+	[Binding, Scope(Tag = "Product:WERCSmart_Page:NewProducts_Tab:ReviewAndSubmit_Section:Summary")]
+
+	class SummaWERCSmart_Distributor_NewProducts_ReviewAndSubmit_Summary
+	{
+		[StepDefinition(@"In the Summary Page, the '(.*)' section should be showing the following value: (.*)")]
+		public void InTheSummarySectionCheckTypeOfProduct(string section, string value)
+		{
+			new Steps_Prototype().ShouldBeShowingFollowing(section, value);
+		}
+	}
+}
