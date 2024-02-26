d32c1c48f25ab58423a85100dde80db0cd922f7a
Turkot, Olha <32143@global.ul.com>
2024-02-26 08:29:23 -0600

    Added step

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/Steps_Prototype.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/Steps_Prototype.cs
@@ -605,6 +605,15 @@ namespace UL.Selenium.Portal.WERCSmart.Steps
 			}
 
 		}
+		[StepDefinition(@"(.*) should be showing the following option: (.*)")]
+		public void ShouldBeShowingFollowing(string section, string option)
+		{
+			var dataSummarySheet = new DataSummary();
+			List<string> found = dataSummarySheet.GetInfoForSectionOption(section, option);
+			Report.IsTrue(found.Contains(option),
+					$"Option was not as expected! Expected: {option} in section: {section} but got: {string.Join(",", found)}",
+					$"Option was showing: {option} in section: {section}");
+		}
 
 	}
 }
