062cadc3891b2cdea852c4d3214b218fa71a444a
Turkot, Olha <32143@global.ul.com>
2024-08-14 09:12:55 -0500

    Updates

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/Steps_ProductGrid.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/Steps_ProductGrid.cs
@@ -12,6 +12,7 @@ using UL.Selenium.Portal.WERCSmart.Selenium_Classes.New_Product;
 using UL.Selenium.Portal.WERCSmart.Classes;
 using UL.Automation.Reporting.Classes;
 using UL.Automation.ReqnrollHelpers.Attributes;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace UL.Selenium.Portal.WERCSmart.Steps
 {
