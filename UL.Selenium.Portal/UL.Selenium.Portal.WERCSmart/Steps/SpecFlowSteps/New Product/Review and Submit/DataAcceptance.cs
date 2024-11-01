a24b354c0a98c75e793c0e6e8c8741d07215681d
Turkot, Olha <32143@global.ul.com>
2024-11-01 16:33:48 -0500

    Updates

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/New Product/Review and Submit/DataAcceptance.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/New Product/Review and Submit/DataAcceptance.cs
@@ -22,6 +22,12 @@ namespace UL.Selenium.Portal.WERCSmart.Steps.SpecFlowSteps.New_Product.Review_an
 			string checkbox = "Agreed";
 			new Steps_Prototype().ICheckTheCheckboxWithDescription(checked_unchecked, checkbox);
 		}
+		[RegexStepDefinition(@"In the Data Acceptance Section, (check|uncheck) 'Yes, Agreed' checkbox")]
+		public void CheckUncheckYesAgreedCheckbox(string checked_unchecked)
+		{
+			string checkbox = "Yes, Agreed";
+			new Steps_Prototype().ICheckTheCheckboxWithDescription(checked_unchecked, checkbox);
+		}
 		[RegexStepDefinition(@"In the Data Acceptance Section, click 'Summary' button")]
 		public void ClickSummaryButton()
 		{
