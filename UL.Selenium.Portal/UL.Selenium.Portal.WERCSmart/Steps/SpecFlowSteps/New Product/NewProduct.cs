2f346a8f2baee1139e4b5ad3989324f126a90782
Turkot, Olha <32143@global.ul.com>
2025-01-06 13:28:16 -0600

    Updates

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/New Product/NewProduct.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/New Product/NewProduct.cs
@@ -19,11 +19,11 @@ namespace UL.Selenium.Portal.WERCSmart.Steps
 	[Binding, Scope(Tag = "Product:WERCSmart_Page:NewProducts_Tab:ProductType_Section:NewProduct")]
 	class WERCSmart_Distributor_NewProducts_ProductType_NewProduct
 	{
-		[RegexStepDefinition(@"In the New Product Section, set the radio option in section: 'Select the type of product to create': to: (Create a New Registration|Copy from an Existing Registration|Request a UPC from a Manufacturer)")]
+		[RegexStepDefinition(@"In the New Product Section, set the radio option in section: 'Select the type of product to create': to: '(Create Formulated Registration using SDS Import|Create Formulated Registration |Create an Article Registration |Create a Registration by Copying an Existing Registration)'")]
 		public void SelectProductTypeToCreate(string option)
 		{
-			string section = "Select the type of product to create";
-			new Steps_Prototype().SetRadioOptionInSectionTo(section, option);
+			string section = "Select the type of product to create:";
+			new Steps_ProductPrototype().InSectionSetOption(section, option);
 		}
 		[RegexStepDefinition(@"In the New Product Section, set the radio option in section: 'Would you like to Create a New Product\?': to: (Yes, create a new product)")]
 		public void SelectProductTypeToCreatePharma(string option)
