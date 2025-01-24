f290143a175f24ad386a8689e99b13c20e1bc7d8
Palle, Lahari <88148@global.ul.com>
2025-01-24 01:50:55 -0500

    Alert page methods

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Selenium Classes/AlertsPage.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Selenium Classes/AlertsPage.cs
@@ -35,6 +35,9 @@ namespace UL.Selenium.Portal.WERCSmart.Selenium_Classes
 		IWebElement WpsId => this.ContainerElement.FindElement(By.XPath("//input[contains(@data-bind,'wpsId')]"), 2);
 		IWebElement NotificationDate => this.ContainerElement.FindElement(By.XPath("//input[contains(@data-bind,'date.field')]"), 2);
 		IWebElement ProductName => this.ContainerElement.FindElement(By.XPath("//input[contains(@data-bind,'productName.field')]"), 2);
+		private IWebElement selectDropdown(string label_name) => this.ContainerElement.FindElement(By.XPath($".//label[contains(text(),'{label_name}')]/..//following-sibling::select"), 1);
+		private IWebElement Buttons(string buttonLabel) => this.FindElement(By.XPath($".//button[contains(@class,'btn')][text()='{buttonLabel}']"), 1);
+		IWebElement MoreFiltersButton => this.ContainerElement.FindElement(By.XPath(".//a[contains(@class,'btn')]//span[text()='More Filters']"), 2);
 
 		#endregion
 
@@ -74,6 +77,54 @@ namespace UL.Selenium.Portal.WERCSmart.Selenium_Classes
 			this.ProductName.EnterText(productname);
 		}
 
+		public bool SelectOptionExists(string label)
+		{
+			Report.Info($"Attempting to confirm {label}  select option exists.");
+			return this.selectDropdown(label) != null;
+		}
+
+		public bool SelectDropdownValue(string label, string item)
+		{
+			try
+			{
+				this.selectDropdown(label).Select(item);
+				return true;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+
+		public bool ButtonExists(string buttonLabel)
+		{
+			Report.Info($"Attempting to confirm '{buttonLabel}' button exists.");
+			return this.Buttons(buttonLabel) != null;
+		}
+
+		public bool ButtonClick(string buttonLabel)
+		{
+			Report.Info($"Attempting to click '{buttonLabel}' button.");
+			return this.Buttons(buttonLabel).TryClick();
+		}
+
+		public bool MoreFiltersButtonExists()
+		{
+			Report.Info($"Attempting to confirm More Filters button exists.");
+			return this.MoreFiltersButton != null;
+		}
+
+		public bool MoreFiltersButtonClick()
+		{
+			Report.Info($"Attempting to click More Filters Button button.");
+			return this.MoreFiltersButton.TryClick();
+		}
+
+		public bool CheckArchivedCheckbox()
+		{
+			IWebElement el = this.ContainerElement.FindElement(By.XPath(@".//label[contains(text(),'archived messages')]/following-sibling::input[@type='checkbox']"), 2);
+			return el.TryClick() && GeneralUtilities.Wait_for_load_finish();
+		}
 
 		#endregion
 
--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/Steps_AlertsPage.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/Steps_AlertsPage.cs
@@ -17,26 +17,132 @@ namespace UL.Selenium.Portal.WERCSmart.Steps.SpecFlowSteps
 	[Binding, Scope(Tag = "AlertsPage")]
 	class Steps_AlertsPage
 	{
-		[RegexStepDefinition(@"In the Alerts section, Enter WPS ID: (.*)")]
+		[RegexStepDefinition(@"In the Alerts section, set the 'WPS ID' to: (.*)")]
 		public void EnterWpsID(string wpsId)
 		{
 			Report.StartStep("Verify WPS ID field is displayed");
 			new AlertsPage().WpsIdFieldExists();
-			Report.StartStep("Entering wps id: " + wpsId);
+			Report.StartStep($"Attempting to enter '{wpsId}' into wps id search text input.");
 			new AlertsPage().NotificationDateText(wpsId);
 
 
 		}
 
-		[RegexStepDefinition(@"In the Alerts section, Enter Product Name: (.*)")]
+		[RegexStepDefinition(@"In the Alerts section, set the 'Product Name' to: (.*)")]
 		public void EnterProductName(string productName)
 		{
 			Report.StartStep("Verify Product name field is displayed");
 			new AlertsPage().ProductNameFieldExists();
-			Report.StartStep("Entering product name: " + productName);
+			Report.StartStep($"Attempting to enter '{productName}' into product name search text input.");
 			new AlertsPage().ProductNameText(productName);
 
 
 		}
+
+		[RegexStepDefinition(@"In the Alerts section, set the 'Notification Date' in format yyyy-mm-dd to: (.*)")]
+		public void EnterNotificationDate(string date)
+		{
+			Report.StartStep("Verify Notification Date field is displayed");
+			new AlertsPage().NotificationDateFieldExists();
+			Report.StartStep($"Attempting to enter '{date}' into Notification Date search input.");
+			new AlertsPage().NotificationDateText(date);
+
+		}
+
+		[RegexStepDefinition(@"In the Alerts section, set the option of 'Search Type' to: (.*)")]
+		public void SelectSearchType(string option)
+		{
+			string label = "Search Pattern";
+			new AlertsPage().SelectDropdownValue(label, option);
+		}
+
+		[RegexStepDefinition(@"In the Alerts section, set the option of 'Alert Type' to: (.*)")]
+		public void SelectAlertType(string option)
+		{
+			string label = "Alert Type";
+			var optionExists = new AlertsPage().SelectOptionExists(label);
+			if (!optionExists)
+			{
+				new AlertsPage().MoreFiltersButtonClick();
+			}
+			new AlertsPage().SelectDropdownValue(label, option);
+		}
+
+		[RegexStepDefinition(@"In the Alerts section, set the option of 'Brand Name' to: (.*)")]
+		public void SelectBrandName(string option)
+		{
+			string label = "Brand Name";
+			var optionExists = new AlertsPage().SelectOptionExists(label);
+			if (!optionExists)
+			{
+				new AlertsPage().MoreFiltersButtonClick();
+			}
+			new AlertsPage().SelectDropdownValue(label, option);
+		}
+
+		[RegexStepDefinition(@"In the Alerts section,  click the 'Filter' button")]
+		public void ClickTheFilterButton()
+		{
+			string buttonText = "Filter";
+			new AlertsPage().ButtonClick(buttonText);
+		}
+
+		[RegexStepDefinition(@"In the Alerts section,  click the 'Clear Filter' button")]
+		public void ClickTheClearFilterButton()
+		{
+			string buttonText = "ClearFilter";
+			new AlertsPage().ButtonClick(buttonText);
+		}
+
+		[RegexStepDefinition(@"In the Alerts section,  click the 'Export' button")]
+		public void ClickTheExportButton()
+		{
+			string buttonText = "Export";
+			new AlertsPage().ButtonClick(buttonText);
+		}
+
+		[RegexStepDefinition(@"In the Alerts section, click the ' More Filter' button")]
+		public void ClickTheMoreFilterButton()
+		{
+			new AlertsPage().MoreFiltersButtonClick();
+		}
+
+		[RegexStepDefinition(@"In the Alerts section, (check|uncheck) the show archived checkbox")]
+		public void ICheckTheCheckboxShowArchived(string check)
+		{
+			bool toCheck = false;
+			if (check == "check")
+			{
+				toCheck = true;
+			}
+			else if (check == "uncheck")
+			{
+				toCheck = false;
+			}
+			else
+			{
+				throw new Exception("Specflow paramater must be equal to 'check' or 'uncheck'");
+			}
+			bool isChecked = new AlertsPage().CheckArchivedCheckbox().Checked();
+			if (isChecked == toCheck)
+			{
+				Report.Success($"The checkbox was already {check}ed");
+				return;
+			}
+			Report.IsTrue(new AlertsPage().CheckArchivedCheckbox(),
+				$"Failed to check the checkbox!",
+				$"Successfully checked the checkbox");
+			Report.IsTrue(new AlertsPage().CheckArchivedCheckbox().Checked() == toCheck,
+				$"The checkbox was not {check}ed after",
+				$"The checkbox is {check}ed as expected");
+		}
+		[RegexStepDefinition(@"In the Alerts section, Confirm the checkbox show archived (is|is not) checked")]
+		public void TheCheckboxShowArchivedIsIsNotChecked(string is_isnot)
+		{
+			bool expected = is_isnot == "is";
+			bool isChecked = new AlertsPage().CheckArchivedCheckbox().Checked();
+			Report.IsTrue(isChecked == expected, $"Failed to confirm the checkbox show archives {(expected ? "is not" : "is")} checked'!", $"Successfully confirmed the checkbox checkbox show archives {is_isnot} checked");
+		}
+
 	}
 }
