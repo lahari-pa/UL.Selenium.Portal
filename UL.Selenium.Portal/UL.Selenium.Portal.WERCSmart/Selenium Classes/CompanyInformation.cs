a2db8841119381e8e17eea4d2a3b761817668f16
Turkot, Olha <32143@global.ul.com>
2025-02-03 15:56:37 -0600

    Added steps

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Selenium Classes/CompanyInformation.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Selenium Classes/CompanyInformation.cs
@@ -14,28 +14,28 @@ namespace UL.Selenium.Portal.WERCSmart.Selenium_Classes
 	class CompanyInformation : SeleniumBaseObject
 	{
 		protected override By ContainerElementLocator => By.XPath("//div[@class='body-content']");
-		IWebElement EditLink(string section) => this.ContainerElement.FindElement(By.XPath($"//div[h3[text()='{section}']]//a[text()='Edit']"));
-		IWebElement CompanyEditLink(string section) => this.ContainerElement.FindElement(By.XPath($"//div[@class='row'][div//h3[text()='{section}']]//a[text()='Edit']"));
-		 
-		public bool EditLinkExists(string section)
+		IWebElement LinkElement(string section, string link) => this.ContainerElement.FindElement(By.XPath($".//div[h3[text()='{section}']]//a[text()='{link}']"));
+		IWebElement CompanyLinkElement(string link) => this.ContainerElement.FindElement(By.XPath($".//div[@class='row'][div//span[normalize-space(text()) = 'Company Name']]//a[text()='{link}']"));
+
+		public bool LinkElementExists(string section, string linkText)
 		{
-			Report.Info($"Attempt to find the 'edit' link for section '{section}'");
-			return this.EditLink(section) != null;
+			Report.Info($"Attempt to find the '{linkText}' link for section '{section}'");
+			return this.LinkElement(section, linkText) != null;
 		}
-		public bool ClickEditLink(string section)
+		public bool ClickLinkElement(string section, string linkText)
 		{
-			Report.Info($"Attempt to click the 'edit' link for section '{section}'");
-			return this.EditLink(section).TryClick();
+			Report.Info($"Attempt to click the '{linkText}' link for section '{section}'");
+			return this.LinkElement(section, linkText).TryClick();
 		}
-		public bool CompanyEditLinkExists(string section)
+		public bool CompanyEditLinkExists(string linkText)
 		{
-			Report.Info($"Attempt to find the 'edit' link for section '{section}'");
-			return this.CompanyEditLink(section) != null;
+			Report.Info($"Attempt to find the '{linkText}' link for section 'Company'");
+			return this.CompanyLinkElement(linkText) != null;
 		}
-		public bool CompanyClickEditLink(string section)
+		public bool CompanyClickEditLink(string linkText)
 		{
-			Report.Info($"Attempt to click the 'edit' link for section '{section}'");
-			return this.CompanyEditLink(section).TryClick();
+			Report.Info($"Attempt to click the '{linkText}' link for section 'Company'");
+			return this.CompanyLinkElement(linkText).TryClick();
 		}
 	}
 }
--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/Steps_CompanyInformation.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/Steps_CompanyInformation.cs
@@ -17,19 +17,49 @@ namespace UL.Selenium.Portal.WERCSmart.Steps.SpecFlowSteps
 		[RegexStepDefinition(@"In the Company Information section, click the 'Edit' link for the (Billing Address|Shipping Address|Canada Supplier Address|Stewardship Numbers) section")]
 		public void ClickTheEditLink(string section)
 		{
-			if (Report.IsTrue(new CompanyInformation().EditLinkExists(section), $"Failed to find the 'Edit' link for the '{section}' section", $"Successfully found the 'Edit' link for the '{section}' section"))
+			string linkText = "Edit";
+			if (Report.IsTrue(new CompanyInformation().LinkElementExists(section, linkText), $"Failed to find the 'Edit' link for the '{section}' section", $"Successfully found the 'Edit' link for the '{section}' section"))
 			{
-				Report.IsTrue(new CompanyInformation().ClickEditLink(section), $"Failed to click the 'Edit' link for the '{section}' section", $"Successfully clicked the 'Edit' link for the '{section}' section");
+				Report.IsTrue(new CompanyInformation().ClickLinkElement(section, linkText), $"Failed to click the 'Edit' link for the '{section}' section", $"Successfully clicked the 'Edit' link for the '{section}' section");
 			}
 		}
 		[RegexStepDefinition(@"In the Company Information section, click the 'Edit' link for the 'Company' section")]
 		public void ClickTheCompanyEditLink()
 		{
-			string section = "Company";
-			if (Report.IsTrue(new CompanyInformation().CompanyEditLinkExists(section), $"Failed to find the 'Edit' link for the '{section}' section", $"Successfully found the 'Edit' link for the '{section}' section"))
+			string linkText = "Edit";
+			if (Report.IsTrue(new CompanyInformation().CompanyEditLinkExists(linkText), $"Failed to find the 'Edit' link for the 'Company' section", $"Successfully found the 'Edit' link for the 'Company' section"))
 			{
-				Report.IsTrue(new CompanyInformation().CompanyClickEditLink(section), $"Failed to click the 'Edit' link for the '{section}' section", $"Successfully clicked the 'Edit' link for the '{section}' section");
+				Report.IsTrue(new CompanyInformation().CompanyClickEditLink(linkText), $"Failed to click the 'Edit' link for the 'Company' section", $"Successfully clicked the 'Edit' link for the 'Company' section");
 			}
+		}
+		[RegexStepDefinition(@"In the Company Information section, click the (Save|Cancel) button for the (Billing Address|Shipping Address|Canada Supplier Address|Stewardship Numbers) section")]
+		public void ClickTheSaveCancelButton(string button, string section)
+		{
+			if (Report.IsTrue(new CompanyInformation().LinkElementExists(section, button), $"Failed to find the '{button}' button for the '{section}' section", $"Successfully found the '{button}' button for the '{section}' section"))
+			{
+				Report.IsTrue(new CompanyInformation().ClickLinkElement(section, button), $"Failed to click the '{button}' button for the '{section}' section", $"Successfully clicked the '{button}' button for the '{section}' section");
+			}
+		}
+		[RegexStepDefinition(@"In the Company Information section, click the (Save|Cancel) button for the 'Company' section")]
+		public void ClickTheCompanySaveCancelButton(string button)
+		{
+			if (Report.IsTrue(new CompanyInformation().CompanyEditLinkExists(button), $"Failed to find the '{button}' button for the 'Company' section", $"Successfully found the '{button}' button for the 'Company' section"))
+			{
+				Report.IsTrue(new CompanyInformation().CompanyClickEditLink(button), $"Failed to click the '{button}' button for the 'Company' section", $"Successfully clicked the '{button}' button for the 'Company' section");
+			}
+		}
+		[RegexStepDefinition(@"In the Company Information section, the 'Are you sure you wish to cancel\?' modal window (should|should not) be displayed with text 'If you cancel, any changes will be lost. Continue\?'")]
+		public void TheUserDetailsModalIsDisplayed(string condition)
+		{
+			string modalTitle = "×\r\nAre you sure you wish to cancel?";
+			string text = "If you cancel, any changes will be lost. Continue?";
+			new Steps_Prototype().ThenIConfirmThePopUpShowsTheHeadingAndText(condition, modalTitle, text);
+		}
+		[RegexStepDefinition(@"In the Company Information section, in the 'Are you sure you wish to cancel\?' modal click the button (Yes|No)")]
+		public void TheInUserDetailsModalClickButton(string button)
+		{
+			string modalTitle = "Are you sure you wish to cancel?";
+			new Steps_Prototype().ThenInThePopupViewWithTheFollowingTitleIClickTheButton(modalTitle, button);
 		}
 	}
 }
