27b14501e9cff45fa45a97d473982e60a387ed71
Palle, Lahari <88148@global.ul.com>
2025-02-04 10:48:08 -0500

    MyReports steps

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Selenium Classes/New Product/NewProduct.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Selenium Classes/New Product/NewProduct.cs
@@ -3665,15 +3665,15 @@ namespace UL.Selenium.Portal.WERCSmart.Selenium_Classes.New_Product
 
 		public bool SetOptionInSection(string section, string value)
 		{
-			string xPath = @"(//span[(.//ancestor::div[starts-with(@class,'form-group')]//label[contains(text(),""" + section + @""")]) and contains(text(),""" + value + @""") and (./preceding-sibling::input[@type='checkbox'])]/preceding-sibling::input[@type='checkbox'] | " +
-						@"//span[(.//ancestor::div[starts-with(@class,'form-group')]//label[contains(text(),""" + section + @""")]) and contains(text(),""" + value + @""") and (./preceding-sibling::input[@type='radio'])]/parent::label | " +
-						@"//input[(.//ancestor::div[starts-with(@class,'form-group')]//label[contains(text(),""" + section + @""")]) and @type='text'] | " +
-						@"//input[(.//ancestor::div[starts-with(@class,'form-group')]//label[contains(text(),""" + section + @""")]) and @type='email'] | " +
-						@"//select[(.//ancestor::div[starts-with(@class,'form-group')]//label[contains(text(),""" + section + @""")])] | " +
-						@"//select[contains(@data-bind,""" + section + @""")] |" +
-						@"//span[(.//ancestor::div[starts-with(@class,'form-group')]//label[contains(text(),""" + section + @""")]) and contains(text(),""" + value + @""") and not(.//parent::label[contains(@class,'btn')])]/preceding-sibling::input) |" +
-						@"//select[(.//ancestor::div//label[contains(text(),""" + section + @""")])] |" +
-						@"//input[(.//following-sibling::label[contains(text(),""" + section + @""")])]";
+			string xPath = $"(//span[(.//ancestor::div[starts-with(@class,'form-group')]//label[contains(text(),'{section}')]) and contains(text(),'{value}') and (./preceding-sibling::input[@type='checkbox'])]/preceding-sibling::input[@type='checkbox'] | " +
+						$"//span[(.//ancestor::div[starts-with(@class,'form-group')]//label[contains(text(),'{section}')]) and contains(text(),'{value}') and (./preceding-sibling::input[@type='radio'])]/parent::label | " +
+						$"//input[(.//ancestor::div[starts-with(@class,'form-group')]//label[contains(text(),'{section}')]) and @type='text'] | " +
+						$"//input[(.//ancestor::div[starts-with(@class,'form-group')]//label[contains(text(),'{section}')]) and @type='email'] | " +
+						$"//select[(.//ancestor::div[starts-with(@class,'form-group')]//label[contains(text(),'{section}')])] | " +
+						$"//select[contains(@data-bind,'{section}')] |" +
+						$"//span[(.//ancestor::div[starts-with(@class,'form-group')]//label[contains(text(),'{section}')]) and contains(text(),'{value}') and not(.//parent::label[contains(@class,'btn')])]/preceding-sibling::input) |" +
+						$"//select[(.//ancestor::div//label[contains(text(),'{section}')])] |" +
+						$"//input[(.//following-sibling::label[contains(text(),'{section}')])]";
 
 			IWebElement el = this.ContainerElement.FindElement(By.XPath(xPath), 10);
 
