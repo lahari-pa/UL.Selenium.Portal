710359dd90d95fb4d58d2e77e0f6ad5fa81877ce
Palle, Lahari <88148@global.ul.com>
2025-02-26 00:42:42 -0500

    Ingredients steps

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Selenium Classes/MyLibrary.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Selenium Classes/MyLibrary.cs
@@ -254,7 +254,6 @@ namespace UL.Selenium.Portal.WERCSmart.Selenium_Classes
 		#endregion
 
 
-
 	}
 
 	public class MyIngredientsPage : SeleniumBaseObject
@@ -263,7 +262,12 @@ namespace UL.Selenium.Portal.WERCSmart.Selenium_Classes
 		#region Page Objects
 		protected override By ContainerElementLocator => By.XPath("//div[@class='body-content']");
 		IWebElement ComponentSearchBox => this.ContainerElement.FindElement(By.XPath(".//span[@class='select2-selection__placeholder' and contains(text(),'Start typing a component name to search')]"), 1);
-		IWebElement TableCheckbox(string componentname, string checkboxlabel) => this.ContainerElement.FindElement(By.XPath($".//div[contains(@id,'settings')]//table//tr//td//span[contains(@data-bind,'component.name')][text()='{componentname}']//ancestor::tr//td//input[contains(@data-bind,'checked: {checkboxlabel}')]"), 1);
+		IWebElement TableCheckbox(string componentname, string checkboxlabel) => this.ContainerElement.FindElement(By.XPath($".//div[contains(@id,'settings')]//table//tr//td//span[contains(@data-bind,'component.name')][contains(text(),'{componentname}')]//ancestor::tr//td//input[contains(@data-bind,'checked: {checkboxlabel}')]"), 1);
+		private IWebElement PublicNameDropdown(string componentname) => this.ContainerElement.FindElement(By.XPath($".//div[contains(@id,'settings')]//table//tr//td//span[contains(@data-bind,'component.name')][contains(text(),'{componentname}')]//ancestor::tr//td//select[contains(@data-bind,'publicName')]"), 1);
+		private List<IWebElement> PublicNameDropdownOptionsList(string componentname) => this.PublicNameDropdown(componentname).FindElements(By.XPath("//option"), 1).ToList();
+		private IWebElement PublicNameDropdownOption(string componentname, string dropdownOption) => this.PublicNameDropdownOptionsList(componentname).Where(x => x.Text.Trim() == dropdownOption).FirstOrDefault();
+		IWebElement actionbutton(string name, string action) => this.FindElement(By.XPath($".//div[contains(@id,'settings')]//table//tr//td//span[contains(@data-bind,'component.name')][contains(text(),'{name}')]//ancestor::tr//td//button[@title='{action}']"), 2);
+		IWebElement disclosurebutton(string action) => this.FindElement(By.XPath($".//div[contains(@data-bind,'model')]//table//tr//th//p//a[text()='{action}']"), 2);
 		#endregion
 
 		#region Methods
@@ -301,10 +305,44 @@ namespace UL.Selenium.Portal.WERCSmart.Selenium_Classes
 		}
 
 
+		public bool PublicNameDropdownExists(string componentname)
+		{
+			Report.Info($"Attempting to confirm Public Name dropdown exists for {componentname}.");
+			return this.PublicNameDropdown(componentname) != null;
+		}
+
+		public bool PublicNameDropdownClick(string componentname)
+		{
+			Report.Info($"Attempting to click Public Name dropdown for {componentname}.");
+			return this.PublicNameDropdown(componentname).TryClick();
+		}
 
 
+		public bool PublicNameDropdownOptionExists(string componentname, string dropdownOption)
+		{
+			Report.Info($"Attempting to confirm Public Name dropdown contains '{dropdownOption}' option.");
+			return this.PublicNameDropdownOption(componentname, dropdownOption) != null;
+		}
 
+		public bool PublicNameDropdownOptionClick(string componentname, string dropdownOption)
+		{
+			Report.Info($"Attempting to click Public Name dropdown '{dropdownOption}' option.");
+			return this.PublicNameDropdownOption(componentname, dropdownOption).TryClick();
+		}
 
+		public bool ForIngredientsClickAction(string name, string action)
+		{
+			
+			return this.actionbutton(name, action).TryClick();
+
+		}
+
+		public bool ForBulkProductManagementClickSelectAllOrUnselectAll(string action)
+		{
+
+			return this.disclosurebutton(action).TryClick();
+
+		}
 
 		#endregion
 
