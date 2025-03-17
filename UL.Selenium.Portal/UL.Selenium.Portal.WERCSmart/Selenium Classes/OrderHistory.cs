1e971d53c818c7074e8645e0ca01997d92420bb8
Palle, Lahari <88148@global.ul.com>
2025-03-17 01:22:36 -0400

    Order History steps

---
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Selenium Classes/OrderHistory.cs
@@ -0,0 +1,54 @@
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using UL.Automation.Reporting.Functions;
+using UL.Automation.WebDriver.BaseClasses;
+using UL.Automation.WebDriver.Classes;
+using UL.Automation.WebDriver.Extensions;
+using UL.Automation.WebDriver.Functions;
+
+
+
+namespace UL.Selenium.Portal.WERCSmart.Selenium_Classes
+{
+	public class OrderHistoryPage : SeleniumBaseObject
+	{
+
+		#region Page Objects
+
+		protected override By ContainerElementLocator => By.XPath("//div[@class='body-content']");
+		
+		IWebElement Table => this.ContainerElement.FindElement(By.XPath("//div[contains(@class,'ws-panel')]//table"), 2);
+
+		IWebElement ActionButton(string name, string action) => this.FindElement(By.XPath($".//div[contains(@class,'ws-panel')]//table//tr//td//span[contains(@data-bind,'OrderNumber')][text()='{name}']//ancestor::tr//td//a[text()='{action}']"), 2);
+		#endregion
+
+		#region Page Methods
+
+		public bool TableExists()
+		{
+			Report.Info("Starting looking for Table");
+			return this.Table != null;
+		}
+
+		public bool ClickAction(string name, string action)
+		{
+
+			return this.ActionButton(name, action).TryClick();
+
+		}
+
+
+
+
+
+		#endregion
+
+
+	}
+
+
+
+}
---
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Steps/SpecFlowSteps/Steps_OrderHistory.cs
@@ -0,0 +1,60 @@
+﻿using Reqnroll;
+using UL.Automation.Reporting.Functions;
+using UL.Automation.ReqnrollHelpers.Attributes;
+using UL.Selenium.Portal.WERCSmart.Selenium_Classes;
+
+
+
+namespace UL.Selenium.Portal.WERCSmart.Steps.SpecFlowSteps
+{
+	[Binding, Scope(Tag = "OrderHistoryPage")]
+	class Steps_OrderHistory
+	{
+		[RegexStepDefinition(@"In the Order History section, Click on (Subscription|WERCSmart)")]
+		public void ClickTheOrderHistoryLink(string linkText)
+		{
+			new Steps_Prototype().ClickLinkElement(linkText);
+		}
+
+		[RegexStepDefinition(@"In the Order History section, Click Filter button")]
+		public void ClickFlterButton()
+		{
+			string button = "Filter";
+			new Steps_Prototype().ClickButton(button);
+		}
+
+		[RegexStepDefinition(@"In the Order History section, Click Clear Filter button")]
+		public void ClickClearFlterButton()
+		{
+			string button = "Clear Filter";
+			new Steps_Prototype().ClickButton(button);
+		}
+
+		[RegexStepDefinition(@"In the Order History section, Verify WERCSmart table is displayed")]
+		public void VerifyWERCSmartTableIsDisplayed()
+		{
+			Report.IsTrue(new OrderHistoryPage().TableExists(), $"WERCSmart table is not displayed", $"Successfully WERCSmart table is displayed");
+		}
+
+		[RegexStepDefinition(@"In the Order History section, Verify Subscription table is displayed")]
+		public void VerifySubscriptionTableIsDisplayed()
+		{
+			Report.IsTrue(new OrderHistoryPage().TableExists(), $"Subscription table is not displayed", $"Successfully Subscription table is displayed");
+		}
+
+		[RegexStepDefinition(@"In the Order History section, click action (View details|Close) for order number: (.*)")]
+		public void ClickActionForOrder(string ordernumber, string action)
+		{
+			Report.IsTrue(new OrderHistoryPage().ClickAction(ordernumber, action),
+				$"Failed to click action:{action} for order: {ordernumber}",
+				$"Successfully clicked action: {action} for order: {ordernumber}");
+		}
+
+
+
+	}
+
+
+}
+
+
