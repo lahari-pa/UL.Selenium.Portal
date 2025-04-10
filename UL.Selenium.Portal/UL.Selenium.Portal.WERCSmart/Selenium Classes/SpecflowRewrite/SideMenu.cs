4be5a9c97af4b0beafe6d30f01409e9f6db85dbc
Gregory Ward <25147@global.ul.com>
2025-04-09 20:57:44 -0400

    SideMenu code

--- UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Selenium Classes/SpecflowRewrite/SideMenu.cs
+++ UL.Selenium.Portal/UL.Selenium.Portal.WERCSmart/Selenium Classes/SpecflowRewrite/SideMenu.cs
@@ -15,9 +15,12 @@ namespace UL.Selenium.Portal.WERCSmart.Selenium_Classes.SpecflowRewrite
 		private IWebElement HamburgerIcon => this.FindElement(By.XPath(".//div[contains(@class,'hamburger-to-close')]"), 1);
 		public List<IconLink> IconLinksList => [.. this.FindElements(By.XPath(".//div[@class='sidemenu-icons']//a[@title]"), 1).Select(x => new IconLink(x))];
 		public IconLink IconLinkByTitle(string titleLabel) => this.IconLinksList.FirstOrDefault(x => x.Title.Equals(titleLabel, System.StringComparison.Ordinal));
+		public List<LabeledLink> LabeledLinksList => [.. this.FindElements(By.XPath(".//div[contains(@class,'sidemenu-links')]//a[text()]"), 1).Select(x => new LabeledLink(x))];
+		public LabeledLink LabeledLinkByTitle(string titleLabel) => this.LabeledLinksList.FirstOrDefault(x => x.Title.Equals(titleLabel, System.StringComparison.Ordinal));
 		#endregion
 
 		#region Class Methods
+		#region Hamburger Icon Methods
 		public bool HamburgerIconExists()
 		{
 			Report.Info($"Attempting to confirm Hamburger Icon exists.");
@@ -35,6 +38,45 @@ namespace UL.Selenium.Portal.WERCSmart.Selenium_Classes.SpecflowRewrite
 			Report.Info($"Attempting to confirm Sidebar Menu is expanded.");
 			return this.HamburgerIcon.GetAttribute("class").Contains("closed");
 		}
+		#endregion
+		#region Icon Links List Methods
+		public bool IconLinksListExists()
+		{
+			Report.Info($"Attempting to confirm Icon Links list exists.");
+			return !this.IconLinksList.IsNullOrEmpty();
+		}
+
+		public List<string> IconLinksTitlesList()
+		{
+			Report.Info($"Attempting to get Icon Links titles list.");
+			return [.. this.IconLinksList.Select(x => x.Title)];
+		}
+
+		public bool IconLinkByTitleExists(string titleLabel)
+		{
+			Report.Info($"Attempting to confirm '{titleLabel}' Icon Link exists.");
+			return this.IconLinkByTitle(titleLabel) != null;
+		}
+		#endregion
+		#region Labeled Links List Methods
+		public bool LabeledLinksListExists()
+		{
+			Report.Info($"Attempting to confirm Labeled Links list exists.");
+			return !this.LabeledLinksList.IsNullOrEmpty();
+		}
+
+		public List<string> LabeledLinksTitlesList()
+		{
+			Report.Info($"Attempting to get Labeled Links Titles list.");
+			return [.. this.LabeledLinksList.Select(x => x.Title)];
+		}
+
+		public bool LabeledLinkByTitleExists(string titleLabel)
+		{
+			Report.Info($"Attempting to confirm '{titleLabel}' Labeled Link exists.");
+			return this.LabeledLinkByTitle(titleLabel) != null;
+		}
+		#endregion
 		#endregion
 	}
 
